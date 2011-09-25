67e262ade5e49a89c69115d9c0551cd2289dd80c
Sigurd Holsen <sighol@gmail.com>
2011-09-25 23:13:34 +0200

    added GateKeeper example in NewsFeedController
    added protected/views/site/403.php
    changed protected/controllers/NewsfeedController.php

--- protected/controllers/NewsfeedController.php
+++ protected/controllers/NewsfeedController.php
@@ -13,6 +13,19 @@
 class NewsfeedController extends Controller {
 	
 	public function actionIndex() {
+		$allowed = array(
+				'sex' => "male",
+				'role' => "mod",
+				'group' => 'bedkom',
+				'loggedIn' => true,
+		);
+		
+		// if ( ! GateKeeper::check($allowed)) {
+		if (true) {
+			$this->render("application.views.site.403",array("msg"=> "Dette gikk ikke"));
+			return;
+		}
+		
 		$this->render("index");
 		
 	}
---
+++ protected/views/site/403.php
@@ -0,0 +1,8 @@
+<h1>Error 403</h1>
+
+<p> Du har ikke tilgang til denne siden </p>
+
+<? if (isset($msg)): ?>
+<p><strong>Feilmelding:</strong> <?= $msg ?>
+
+<? endif ?>
