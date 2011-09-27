192137b29a2c4d85d92f24e7d33a5b75fe432ad8
Sigurd Holsen <sighol@gmail.com>
2011-09-27 22:07:46 +0200

    Ryddet opp for å brukes som utganspunkt

--- protected/views/site/error.php
+++ protected/views/site/error.php
@@ -6,7 +6,7 @@ $this->breadcrumbs=array(
 ?>
 
 <h2>Error <?php echo $code; ?></h2>
-
+<i>Denne erroren ble fanget opp i <?= __FILE__ ?></i>
 <div class="error">
 <?php echo CHtml::encode($message); ?>
 </div>
