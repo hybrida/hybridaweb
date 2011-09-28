735f1719c778e038bbd29b256b89a2da510091e2
Sigurd Holsen <sighol@gmail.com>
2011-09-28 10:53:21 +0200

    skiftet rettigheter lokalt, byttet theme, tester ActiveRecord

---
+++ protected/views/news2/_search.php
@@ -0,0 +1,54 @@
+<div class="wide form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'action'=>Yii::app()->createUrl($this->route),
+	'method'=>'get',
+)); ?>
+
+	<div class="row">
+		<?php echo $form->label($model,'id'); ?>
+		<?php echo $form->textField($model,'id'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'parentId'); ?>
+		<?php echo $form->textField($model,'parentId'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'parentType'); ?>
+		<?php echo $form->textField($model,'parentType',array('size'=>7,'maxlength'=>7)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'title'); ?>
+		<?php echo $form->textField($model,'title',array('size'=>50,'maxlength'=>50)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'imageId'); ?>
+		<?php echo $form->textField($model,'imageId'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'content'); ?>
+		<?php echo $form->textArea($model,'content',array('rows'=>6, 'cols'=>50)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'author'); ?>
+		<?php echo $form->textField($model,'author'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'timestamp'); ?>
+		<?php echo $form->textField($model,'timestamp'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton('Search'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- search-form -->
