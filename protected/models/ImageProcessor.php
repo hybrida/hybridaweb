d7e01adf24b1386ae3667e907b4213a354d38a98
Bjørnar Askevold <bjorask@stud.ntnu.no>
2011-10-18 19:04:21 +0200

    ImageController lagt til, men virker ikke enda

---
+++ protected/models/ImageProcessor.php
@@ -0,0 +1,42 @@
+<?php
+class ImageProcessor  {
+    
+    function getFileExtension($src) {
+		return substr(strrchr($src,'.'),1);
+	}
+
+	function resizeImage($src, $newWidth, $newHeight) {
+
+		list($oldWidth, $oldHeight) = getimagesize($src);
+		if( $oldWidth > $oldHeight ) {
+			$realWidth = $newWidth;
+			$realHeight = floor( $oldHeight/$oldWidth*$newWidth );
+			$y = floor( ($newHeight - $realHeight) / 2 );
+			$x = 0;
+		} else {
+			$realWidth = floor( $oldWidth/$oldHeight*$newHeight );
+			$realHeight = $newHeight;
+			$x = floor( ($newWidth - $realWidth) / 2 );
+			$y = 0;
+		}
+		
+		switch( getFileExtension($src) ){
+			case "png":
+				$img = ImageCreateFrompng($src);
+				break;
+			case "jpeg":
+				$img = ImageCreateFromjpeg($src);
+				break;
+			case "jpg":
+				$img = ImageCreateFromjpeg($src);
+		}
+		
+		$newImg = ImageCreateTrueColor($newWidth, $newHeight);
+		imagecopyresized($newImg, $img, $x, $y, 0, 0, $realWidth, $realHeight, $oldWidth, $oldHeight);
+		return $newImg;
+	}
+
+	function createThumbnail($src,$x,$y,$dst){
+		imagejpeg(resizeImage($src, $x, $y), $dst);
+	}
+}
