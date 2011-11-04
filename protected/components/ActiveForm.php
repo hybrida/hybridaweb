a41b347706d550399943d125e1c7e8f2de554086
Sigurd Holsen <sighol@gmail.com>
2011-11-04 12:04:41 +0100

    Nå fungerer textArea

--- protected/components/ActiveForm.php
+++ protected/components/ActiveForm.php
@@ -2,18 +2,29 @@
 
 class ActiveForm extends CActiveForm {
 
-	public function textArea($model, $tablePostName) {
+	public function textArea($model, $attribute, $htmlOptions=array()) {
+
+		CHtml::resolveNameID($model, $attribute, $htmlOptions);
+		//CHtml::clientChange('change', $htmlOptions);
+		if ($model->hasErrors($attribute))
+			CHtml::addErrorCss($htmlOptions);
+		$text = CHtml::resolveValue($model, $attribute);
+
+
+		$class = get_class($model);
+		$name = $class . "[" . $attribute . "]";
+
 		$this->widget('application.components.widgets.XHeditor', array(
-				'language' => 'en', //options are en, zh-cn, zh-tw
-				'config' => array(
-						'id' => 'facebook'  . rand(0, 100000), // FIXME
-						'name' => $tablePostName,
-						'tools' => 'mini', // mini, simple, full or from XHeditor::$_tools, tool names are case sensitive
-						'width' => '70%',
-				//see XHeditor::$_configurableAttributes for more
-				),
-				'contentValue' => '', // default value displayed in textarea/wysiwyg editor field
-				'htmlOptions' => array('rows' => 5, 'cols' => 10), // to be applied to textarea
+			'language' => 'en', //options are en, zh-cn, zh-tw
+			'config' => array(
+				'id' => $htmlOptions['id'],
+				'name' => $htmlOptions['name'],
+				'tools' => 'mini', // mini, simple, full or from XHeditor::$_tools, tool names are case sensitive
+				'width' => '70%',
+			//see XHeditor::$_configurableAttributes for more
+			),
+			'contentValue' => $text, // default value displayed in textarea/wysiwyg editor field
+			'htmlOptions' => array('rows' => 5, 'cols' => 10), // to be applied to textarea
 		));
 		return false;
 	}
