d7dfd5abeb8a911dbeb7f078792377430d7a7564
Sigurd Holsen <sighol@gmail.com>
2011-11-17 12:37:23 +0100

    flyttet innsida.crt inn i prosjektet for testing på egen maskin

--- protected/components/SSOclient.php
+++ protected/components/SSOclient.php
@@ -1,7 +1,7 @@
 <?php
 class SSOclient {
   var $loginvalues;
-  var $crtfile = "/var/www/innsida.crt";
+  var $crtfile = "/var/www/yii/innsida.crt";
   var $verifies;
   var $oktime;
   var $reason;
