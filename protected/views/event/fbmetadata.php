c95495ca5c763132ae6d11190a71e9eb458095ba
Bjørnar Askevold <bjorask@stud.ntnu.no>
2011-11-17 15:33:22 +0100

    rettet til event på eventmetadata

--- protected/views/event/fbmetadata.php
+++ protected/views/event/fbmetadata.php
@@ -1,6 +1,6 @@
 <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# lfhybrida: http://ogp.me/ns/fb/lfhybrida#">
       <meta property="fb:app_id"      content="202808609747231" /> 
-      <meta property="og:type"        content="lfhybrida:company_presentation" /> 
+      <meta property="og:type"        content="lfhybrida:event" /> 
       <meta property="og:url"         content="http://appletini.ivt.ntnu.no<?= Yii::app()->baseURL ?>/event/facebook/<?= $id ?>" /> 
       <meta property="og:title"       content="<?= $title ?>" /> 
       <meta property="og:description" content="<?= $content ?>" /> 
