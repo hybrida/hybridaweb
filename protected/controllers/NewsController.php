cf71c8b0d178ecb4d04190e401cde134ed200045
Sigurd Holsen <sighol@gmail.com>
2011-11-17 12:40:07 +0100

    Startet på å fikse login via link yii/site/login istedenfor innsida.ntnu....

--- protected/controllers/NewsController.php
+++ protected/controllers/NewsController.php
@@ -16,7 +16,20 @@ class NewsController extends Controller {
 		$this->actionFeed();
 	}
 
-	//put your code here
+	public function filters() {
+		return array(
+			'accessControl',
+		);
+	}
+
+	public function accessRules() {
+		echo "AccessRules";
+		return array(
+			array('deny',
+				'actions' => array("view"),
+			),
+		);
+	}
 
 	public function actionView($id) {
 		$sql = "SELECT title, n.imageId, content, firstName, middleName, lastName, timestamp
@@ -30,9 +43,6 @@ class NewsController extends Controller {
 		if ($data == array()) {
 			throw new CHttpException("Nyheten finnes ikke");
 		}
-
-
-
 		$this->render("view", $data);
 	}
 
@@ -75,7 +85,9 @@ class NewsController extends Controller {
 
 		if (isset($_POST['NewsEventForm'])) {
 			$input = $_POST['NewsEventForm'];
-			echo "<pre>";print_r($input); echo "</pre>";
+			echo "<pre>";
+			print_r($input);
+			echo "</pre>";
 			$model->setAttributes($input);
 			$model->printFields();
 			$model->save();
@@ -83,7 +95,7 @@ class NewsController extends Controller {
 			echo "eventId: " . $model->getEventModel()->id;
 			echo "signupId: " . $model->getSignupModel()->primaryKey;
 
-			//$this->redirectAfterEdit($model);
+			$this->redirectAfterEdit($model);
 			return;
 		}
 
