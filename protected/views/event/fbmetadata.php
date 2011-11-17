134afc6e1e8e7fe9f1b76198e9a4247c12d6f5f4
Bjørnar Askevold <bjorask@stud.ntnu.no>
2011-11-17 14:16:59 +0100

    feil url

--- protected/views/event/fbmetadata.php
+++ protected/views/event/fbmetadata.php
@@ -1,7 +1,7 @@
 <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# lfhybrida: http://ogp.me/ns/fb/lfhybrida#">
       <meta property="fb:app_id"      content="202808609747231" /> 
       <meta property="og:type"        content="lfhybrida:company_presentation" /> 
-      <meta property="og:url"         content="http://appletini.ivt.ntnu.no<?= Yii::app()->baseURL ?>/event/<?= $id ?>" /> 
+      <meta property="og:url"         content="http://appletini.ivt.ntnu.no<?= Yii::app()->baseURL ?>/event/facebook/<?= $id ?>" /> 
       <meta property="og:title"       content="<?= $title ?>" /> 
       <meta property="og:description" content="<?= $content ?>" /> 
       <meta property="og:image"       content="https://s-static.ak.fbcdn.net/images/devsite/attachment_blank.png" />
