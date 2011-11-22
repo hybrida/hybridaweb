7021e619161ec06b59de0f5bcdd1ad82c4a62fc4
Sigurd Holsen <sighol@gmail.com>
2011-11-22 17:41:23 +0100

    fjernet unødvendig mellomrom i layout-filen

--- themes/hybrida/views/layouts/main.php
+++ themes/hybrida/views/layouts/main.php
@@ -10,11 +10,11 @@
 		<link rel="stylesheet" type="text/css" href="<?= Yii::app()->request->baseUrl ?>/style/style.css" />
         <link rel="stylesheet" type="text/css" href="<?= Yii::app()->request->baseUrl ?>/style/hintList.css" />
 
-		<link rel="stylesheet" type="text/css" href="<?= Yii::app()->request->baseUrl ?>/style/layout.css" />
-		<link rel = 'stylesheet' type = 'text/css' href = '<?= Yii::app()->request->baseUrl ?>/style/destroy.css'/>
+		<link rel="stylesheet" type="text/css" href="<?=Yii::app()->request->baseUrl ?>/style/layout.css" />
+		<link rel='stylesheet' type='text/css' href='<?=Yii::app()->request->baseUrl ?>/style/destroy.css'/>
 		<link rel="stylesheet" type="text/css" href="<?=Yii::app()->request->baseUrl ?>/css/form.css" />
 		<link rel="stylesheet" type="text/css" href="<?=Yii::app()->request->baseUrl ?>/fonts/fonts.css" />
-		<link rel = 'stylesheet' type = 'text/css' href="<?=Yii::app()->request->baseUrl?>/style/pageContent.css" />
+		<link rel='stylesheet' type='text/css' href="<?=Yii::app()->request->baseUrl ?>/style/pageContent.css" />
 	</head>
 
 	<body>
