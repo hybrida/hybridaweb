31403408b9c7c7c71c00c79d58d40ac3fd82de7b
Sigurd Holsen <sighol@gmail.com>
2011-11-22 17:48:06 +0100

    fjernet gammel kode i sitecontroller

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -2,37 +2,10 @@
 
 class SiteController extends Controller {
 
-	/**
-	 * Declares class-based actions.
-	 */
-	public function actions() {
-		return array(
-			// captcha action renders the CAPTCHA image displayed on the contact page
-			'captcha' => array(
-				'class' => 'CCaptchaAction',
-				'backColor' => 0xFFFFFF,
-			),
-			// page action renders "static" pages stored under 'protected/views/site/pages'
-			// They can be accessed via: index.php?r=site/page&view=FileName
-			'page' => array(
-				'class' => 'CViewAction',
-			),
-		);
-	}
-
-	/**
-	 * This is the default 'index' action that is invoked
-	 * when an action is not explicitly requested by users.
-	 */
 	public function actionIndex() {
 		//$this->render('index');
         $this->render('../news/feed');
 	}
-
-	/*
-	  public function actionFaq() {
-	  $this->render('faq');
-	  } */
 
 	/**
 	 * This is the action to handle external exceptions.
@@ -46,26 +19,6 @@ class SiteController extends Controller {
 		}
 	}
 
-	/**
-	 * Displays the contact page
-	 */
-	public function actionContact() {
-		$model = new ContactForm;
-		if (isset($_POST['ContactForm'])) {
-			$model->attributes = $_POST['ContactForm'];
-			if ($model->validate()) {
-				$headers = "From: {$model->email}\r\nReply-To: {$model->email}";
-				mail(Yii::app()->params['adminEmail'], $model->subject, $model->body, $headers);
-				Yii::app()->user->setFlash('contact', 'Thank you for contacting us. We will respond to you as soon as possible.');
-				$this->refresh();
-			}
-		}
-		$this->render('contact', array('model' => $model));
-	}
-
-	/**
-	 * Displays the login page
-	 */
 	public function actionLogin($data=null, $sign=null, $target=null) {
 		if ($data == null && $sign == null && $target == null) {
 			$this->redirect("https://innsida.ntnu.no/sso/?target=hybridaweb&returnargs=/site/login");
@@ -82,28 +35,8 @@ class SiteController extends Controller {
 		}
 	}
 
-	/**
-	 * Logs out the current user and redirect to homepage.
-	 */
 	public function actionLogout() {
 		Yii::app()->user->logout();
 		$this->redirect(Yii::app()->homeUrl);
 	}
-
-	public function actionDebug() {
-		if (Yii::app()->user->isGuest) {
-			$this->render("403", array("msg" => "Du må være logget inn!"));
-		} else {
-
-			$user = Yii::app()->user;
-
-			$data = array();
-			$data['username'] = $user->name;
-			$data['id'] = $user->id;
-			$data['access'] = $user->access;
-
-			$this->render("debug", $data);
-		}
-	}
-
-}
+ }
