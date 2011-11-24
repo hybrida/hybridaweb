71100add4e2806a07c632bc27fd1fff73832a1df
Sigurd Holsen <sighol@gmail.com>
2011-11-24 11:54:05 +0100

    La til støtte for å bruke vanlige funksjoner for å hente ut Yii info
    La til en fil globals.php som inneholder korte små funksjoner som gjør at vi
    slipper å skrive så veldig mye.
    
    F.eks kan vi nå skrive
            app() istedenfor Yii::app()
            user() istedenfor Yii::app()->user
            param($name) istedenfor Yii::app()->params[$name]

---
+++ globals.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This is the shortcut to DIRECTORY_SEPARATOR
+ */
+defined('DS') or define('DS',DIRECTORY_SEPARATOR);
+ 
+/**
+ * This is the shortcut to Yii::app()
+ */
+function app()
+{
+    return Yii::app();
+}
+ 
+/**
+ * This is the shortcut to Yii::app()->user.
+ */
+function user() 
+{
+    return Yii::app()->getUser();
+}
+ 
+/**
+ * Returns the named application parameter.
+ * This is the shortcut to Yii::app()->params[$name].
+ */
+function param($name) 
+{
+    return Yii::app()->params[$name];
+}
