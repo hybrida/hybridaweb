f573f9aefc05bbc1221d6a0aaac3001f56cabfb4
Bjørnar Askevold <bjorask@stud.ntnu.no>
2011-11-24 16:57:33 +0100

    rettet opp link i fbmetadata

--- protected/views/event/fbmetadata.php
+++ protected/views/event/fbmetadata.php
@@ -1,12 +1,12 @@
 <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# lfhybrida: http://ogp.me/ns/fb/lfhybrida#">
       <meta property="fb:app_id"      content="202808609747231" /> 
       <meta property="og:type"        content="lfhybrida:event" /> 
-      <meta property="og:url"         content="http://appletini.ivt.ntnu.no<?= Yii::app()->baseURL ?>/event/facebook/<?= $id ?>" /> 
+      <meta property="og:url"         content="http://dev.hybrida.no<?= Yii::app()->baseURL ?>/event/facebook/<?= $id ?>" /> 
       <meta property="og:title"       content="<?= $title ?>" /> 
       <meta property="og:description" content="<?= $content ?>" /> 
       <meta property="og:image"       content="https://s-static.ak.fbcdn.net/images/devsite/attachment_blank.png" />
       
 
 <script type="text/javascript">
-    window.location = "http://appletini.ivt.ntnu.no<?= Yii::app()->baseURL ?>/event/view/<?= $id ?>"
+    window.location = "http://dev.hybrida.no<?= Yii::app()->baseURL ?>/event/<?= $id ?>"
 </script>
