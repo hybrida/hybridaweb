d36399e5efd64ca554a9e0d79ceb28c518fd07a6
Bjørnar Askevold <bjorask@stud.ntnu.no>
2011-11-24 16:56:03 +0100

    fbevent henter content fra news

--- protected/controllers/EventController.php
+++ protected/controllers/EventController.php
@@ -48,8 +48,8 @@ class EventController extends Controller {
 	}
     
     public function actionFacebook( $id ) {
-        $sql = "SELECT id, title, start, end, imageId, location, content 
-		FROM event AS e WHERE id = ?";
+        $sql = "SELECT e.id, e.title, e.start, e.end, e.imageId, e.location, n.content 
+		FROM event AS e LEFT JOIN news AS n ON n.parentId = e.id WHERE e.id = ?";
 
         $command = Yii::app()->db->createCommand($sql);
         $query = $command->query(array($id));
