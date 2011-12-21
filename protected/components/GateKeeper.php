9ef6d02867160457a3ad8f66603669b794db80a4
Sigurd Holsen <sighol@gmail.com>
2011-12-22 00:03:14 +0100

    Refaktorerte GateKeepeer

--- protected/components/GateKeeper.php
+++ protected/components/GateKeeper.php
@@ -1,50 +1,39 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of GateKeeper
- *
- * @author sigurd
- */
 class GateKeeper {
-
-	private static  $userId;
+	private static $userId;
 	private static $access;
-	private static $pdo;
 	private static $isGuest;
 
 	private static function init() {
 		self::$isGuest = Yii::app()->user->isGuest;
-		self::$userId = 1;
-		self::$access = array();
+		self::initLoggedOut();
+		self::initLoggedInn();
+	}
 
-		if ( ! self::$isGuest) {
+	private static function initLoggedOut() {
+		if (self::$isGuest) {
+			self::$userId = 1;
+			self::$access = array();
+		}
+	}
+
+	private static function initLoggedInn() {
+		if (!self::$isGuest) {
 			self::$userId = Yii::app()->user->id;
 			self::$access = Yii::app()->user->access;
 		}
-		self::$pdo = Yii::app()->db->getPdoInstance();
-		
 	}
 
 	public static function hasAccess($type, $id) {
 		self::init();
-
 		$accessRelation = new AccessRelation($type, $id);
 		$access = $accessRelation->get();
 		foreach ($access as $ac) {
-			if (! in_array($ac, self::$access)) {
-				return false;				
+			if (!in_array($ac, self::$access)) {
+				return false;
 			}
 		}
-		
 		return true;
-		
-		
 	}
-
-
 }
