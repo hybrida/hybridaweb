12638cc299e7f24f15cf4fcfc56cd474c706ee09
Bjørnar Askevold <bjorask@stud.ntnu.no>
2011-12-27 23:56:05 +0100

    brukersøk i escript

--- scripts/e.js
+++ scripts/e.js
@@ -84,7 +84,7 @@ function Search(dn) {
 		} else if(lastvalue != input.value) {
 			xhr.request({
 				'type': 'GET',
-				'url': url+file+'/?q=' + input.value + "&type=" + responseURL,
+				'url': url+file+'/?q=' + input.value + "&response=" + responseURL,
 				'functions': {
 					'onload': function() {
 						var response = this.responseText.split(split);
