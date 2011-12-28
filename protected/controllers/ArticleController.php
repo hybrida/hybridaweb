442c3572be66bd6aa4ae54b4f333f5a52047dca1
Bjørnar Askevold <bjorask@stud.ntnu.no>
2011-12-29 00:20:57 +0100

    støtte for artikkelsamlinger

--- protected/controllers/ArticleController.php
+++ protected/controllers/ArticleController.php
@@ -2,14 +2,29 @@
 
 class ArticleController extends Controller {
 
-	public function actionView($id) {
-		$model = Article::model()->findByPk($id);
-		if ($model !== null) {
-			$data = $model->getAttributes();
+    public function actionView($id) {
+        $model = Article::model()->findByPk($id);
+        if ($model !== null) {
+                $data = $model->getAttributes();
 
-			$this->render("view", $data);
-		}
-	}
+                $this->render("view", $data);
+        }
+    }
+    
+    public function actionCollection(){
+
+        $article = new Article();
+        $articleList['menuelements'] = $article->listArticles('wiki');      //Litt for statisk
+
+        $aId = $articleList[0][0];
+
+        $data = $article->getArticle($aId);
+
+        //print_r($data);
+        $this->renderPartial('menu',$articleList);
+        $this->render('view',$data);
+    }
+    
     public function actionEdit($id){
 		$model = $this->getArticleModel($id);
 		
--- protected/views/article/menu.php
+++ protected/views/article/menu.php
@@ -1,6 +1,20 @@
-		<div id='groupNavigation'>
-    		<a class='groupNavigationItem' href=''>Styret</a>
-            <a class='groupNavigationItem' href=''>Kontakt</a>
-            <a class='groupNavigationItem' href=''>Statutter</a>
-            <a class='groupNavigationItem' href=''>Internavis</a>
-		</div>
+<? if(isset($menuelements)) : ?>
+    
+<div id='groupNavigation'>
+    <? foreach($menuelements as $element) : ?>
+        <a class='groupNavigationItem' href='<?= Yii::app()->baseUrl ?>/article/view/<?= $id ?>/<?= $element['title'] ?>'><?= $element['title']?></a>
+    <? endforeach; ?>
+
+    <? if($isAdmin) : ?>
+        <a class='groupNavigationItem' href='<?= Yii::app()->baseUrl ?>/article/edit/<?= $id ?>'>endre</a>
+    <? endif; ?>
+</div>
+
+<? endif; ?>
+
+<div id='groupNavigation'>
+    <a class='groupNavigationItem' href=''>Styret</a>
+    <a class='groupNavigationItem' href=''>Kontakt</a>
+    <a class='groupNavigationItem' href=''>Statutter</a>
+    <a class='groupNavigationItem' href=''>Internavis</a>
+</div>
