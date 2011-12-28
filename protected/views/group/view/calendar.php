fe3555ad4e8131c6436b1ec7fab120c9120563d9
Frans Erstad <franse@stud.ntnu.no>
2011-12-28 18:50:59 +0100

    lagt til spørring lokalt for bedriftsoversikta

--- protected/views/group/view/calendar.php
+++ protected/views/group/view/calendar.php
@@ -16,4 +16,4 @@
 
 <p>Google Calendar for Hybrida. Kalenderen viser planlagte hendelser for hele Hybrida.</p>
 
-<iframe src="https://www.google.com/calendar/embed?src=qiosf3me88g5l6tlp94rsh74a4%40group.calendar.google.com&ctz=Europe/Oslo" style="border: 0" width=700 height=600 frameborder="2" scrolling="yes"/>
+<iframe src="https://www.google.com/calendar/embed?src=qiosf3me88g5l6tlp94rsh74a4%40group.calendar.google.com&ctz=Europe/Oslo" style="border: 0" width="100%" height="75%" frameborder="2" scrolling="yes"/>
