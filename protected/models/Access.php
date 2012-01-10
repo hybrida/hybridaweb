bb422f41cea52739a59e0278c9ff324a1f637d33
Sigurd Holsen <sighol@gmail.com>
2012-01-11 00:56:22 +0100

    Init nytt access-system.
    User har fått nytt api for å hente ut sin access_tabell fra de respektive
    tabellene access-infoen egentlig hører til. Henter kjønn fra brukertabell,
    gruppemedlemskap fra gruppetabell o.s.v.
    
    Dette skal på sikt lagres i Session, så behøvs bare å laster èn gang for hver
     login.

--- protected/models/Access.php
+++ protected/models/Access.php
@@ -3,10 +3,17 @@
 class Access {
 
 	private static $pdo;
+	
+	const REGISTERED = 2;
 
 	const MEMBER = 3;
-	const MALE = 4;
-	const FEMALE = 5;
+	const MALE = 1001;
+	const FEMALE = 1002;
+	
+	const GROUP_START = 4000;
+	const GENDER_START = 1000;
+	const GRADUATION_YEAR_START = 2000;
+	const SPECIALIZATION_START = 3000;
 
 	public function __construct() {
 		self::$pdo = Yii::app()->db->getPdoInstance();
