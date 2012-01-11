34df49985db74e160aaeb8fc68a4decc3cc1f13b
Frans Erstad <franse@stud.ntnu.no>
2012-01-12 00:00:11 +0100

    bk: gjorde ferdig skjemaet for redigering av alumnistudenter

--- protected/modules/bk/models/Bkforms.php
+++ protected/modules/bk/models/Bkforms.php
@@ -2,10 +2,6 @@
 
 class Bkforms {
     
-    public function isCompanyCommentEmpty($comment){
-        return ($comment == '' ? true : false);
-    }
-    
     public function addCompanyComment($comment, $id){
         $this->pdo = Yii::app()->db->getPdoInstance();
 
@@ -31,6 +27,165 @@ class Bkforms {
         );
         $sql = "INSERT INTO bk_company_update (relevantForUserId, companyId, description, addedById, dateAdded) 
 		VALUES (:relevantUserId, :companyId, 'En kommentar er lagt til om bedriften', :currentUserId, now())";
+
+        $query = $this->pdo->prepare($sql);
+        $query->execute($data);
+    }
+    
+    public function deleteAllUpdatesRelevantToCurrentUser(){
+        $this->pdo = Yii::app()->db->getPdoInstance();
+
+        $data = array(
+            'currentUserId' => Yii::app()->user->id
+        );
+        $sql = "DELETE FROM bk_company_update WHERE relevantForUserId = :currentUserId";
+
+        $query = $this->pdo->prepare($sql);
+        $query->execute($data);
+    }
+    
+    public function deleteUpdateByUpdateId($id){
+        $this->pdo = Yii::app()->db->getPdoInstance();
+
+        $data = array(
+            'updateId' => $id
+        );
+        $sql = "DELETE FROM bk_company_update WHERE updateId = :updateId";
+
+        $query = $this->pdo->prepare($sql);
+        $query->execute($data);
+    }
+    
+    public function isCompanyInDatabase($companyName){
+        $this->pdo = Yii::app()->db->getPdoInstance();
+
+        $data = array();
+        $sql = "SELECT companyName FROM bk_company ORDER BY companyName";
+
+        $query = $this->pdo->prepare($sql);
+        $query->execute($data);
+
+        $data = $query->fetchAll(PDO::FETCH_ASSOC);
+        
+        foreach ($data as $company) :
+            if($company['companyName'] == $companyName){
+                return true;
+            }
+        endforeach;
+    }
+    
+    public function isInputFieldEmpty($inputvariable){
+        return ($inputvariable == '' ? true : false);
+    }
+    
+    public function updateGraduateAltEmail($id, $altEmail){
+        $this->pdo = Yii::app()->db->getPdoInstance();
+
+        $data = array(
+            'graduateId' => $id,
+            'altEmail' => $altEmail
+        );
+        $sql = "UPDATE user_new SET altEmail = :altEmail WHERE id = :graduateId";
+
+        $query = $this->pdo->prepare($sql);
+        $query->execute($data);
+    }
+    
+    public function updateGraduateSpecialization($id, $specializationId){
+        $this->pdo = Yii::app()->db->getPdoInstance();
+
+        $data = array(
+            'graduateId' => $id,
+            'specializationId' => $specializationId
+        );
+        $sql = "UPDATE user_new SET specialization = :specializationId WHERE id = :graduateId";
+
+        $query = $this->pdo->prepare($sql);
+        $query->execute($data);
+    }
+    
+    public function updateGraduateWorkDescription($id, $workDescription){
+        $this->pdo = Yii::app()->db->getPdoInstance();
+
+        $data = array(
+            'graduateId' => $id,
+            'workDescription' => $workDescription
+        );
+        $sql = "UPDATE user_new SET workDescription = :workDescription WHERE id = :graduateId";
+
+        $query = $this->pdo->prepare($sql);
+        $query->execute($data);
+    }
+    
+    public function updateGraduateWorkPlace($id, $workPlace){
+        $this->pdo = Yii::app()->db->getPdoInstance();
+
+        $data = array(
+            'graduateId' => $id,
+            'workPlace' => $workPlace
+        );
+        $sql = "UPDATE user_new SET workPlace = :workPlace WHERE id = :graduateId";
+
+        $query = $this->pdo->prepare($sql);
+        $query->execute($data);
+    }
+    
+    public function updateGraduateGraduationYear($id, $graduationYear){
+        $this->pdo = Yii::app()->db->getPdoInstance();
+
+        $data = array(
+            'graduateId' => $id,
+            'graduationYear' => $graduationYear
+        );
+        $sql = "UPDATE user_new SET graduationYear = :graduationYear WHERE id = :graduateId";
+
+        $query = $this->pdo->prepare($sql);
+        $query->execute($data);
+    }
+    
+    public function hasGraduateWorkCompanyChanged($id, $companyName){
+        $this->pdo = Yii::app()->db->getPdoInstance();
+
+        $data = array(
+            'graduateId' => $id
+        );
+        $sql = "SELECT companyName FROM bk_company, user_new 
+                WHERE id = :graduateId AND workCompanyID = companyID";
+
+        $query = $this->pdo->prepare($sql);
+        $query->execute($data);
+
+        $data = $query->fetchAll(PDO::FETCH_ASSOC);
+        
+        foreach ($data as $company) :
+            if($company['companyName'] != $companyName){
+                return true;
+            }
+        endforeach;
+    }
+    
+    public function updateGraduateWorkCompany($id, $companyName){
+        $data = array(
+            'graduateId' => $id,
+            'companyName' => $companyName
+        );
+        $sql = "UPDATE user_new SET workCompanyID = companyID WHERE id = :graduateId AND companyName = :companyName";
+
+        $query = $this->pdo->prepare($sql);
+        $query->execute($data);
+    }
+    
+    public function addCompanyGraduateUpdate($relevantUserId, $companyName){
+        $this->pdo = Yii::app()->db->getPdoInstance();
+
+        $data = array(
+            'companyName' => $companyName,
+            'currentUserId' => Yii::app()->user->id,
+            'relevantUserId' => $relevantUserId
+        );
+        $sql = "INSERT INTO bk_company_update (relevantForUserId, companyId, description, addedById, dateAdded) 
+		VALUES (:relevantUserId, companyID, 'En alumnistudent er knyttet til om bedriften', :currentUserId, now())
+                FROM bk_company WHERE companyName = :companyName";
 
         $query = $this->pdo->prepare($sql);
         $query->execute($data);
