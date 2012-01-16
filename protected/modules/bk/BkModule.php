788950688e8425ef2add452a7673b6f295b0b03f
Sigurd Holsen <sighol@gmail.com>
2012-01-16 22:46:01 +0100

    bk: Rettet skrivefeil

--- protected/modules/bk/BkModule.php
+++ protected/modules/bk/BkModule.php
@@ -29,7 +29,7 @@ class BkModule extends CWebModule {
 		$gk = new GateKeeper;
 		$isBedkomMember = $gk->hasAccessToGroup($this->groupId);
 		if (!$isBedkomMember) {
-			throw new CHttpException("Kunn for bedkom-medlemmer");
+			throw new CHttpException("Kun for bedkom-medlemmer");
 		}
 	}
 
