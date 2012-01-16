b5fe5fb2d1a32d0a839c67a1eafaf99fbdc735ee
Sigurd Holsen <sighol@gmail.com>
2012-01-17 00:30:30 +0100

    Fjernet gamle ubrukte views

--- protected/views/news/index.php
+++
@@ -1,5 +0,0 @@
-<h1>Nyheter</h1>
-
-<p>Dette er nyhetssiden. Her vil det komme en liste over de 10 siste nyhetene</p>
-
-<p>Gå inn på <?=CHtml::link("en nyhet",array("news/1")); ?> for å se et eksempel på en enkeltnyhet</p>
