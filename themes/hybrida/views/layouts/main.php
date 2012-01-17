ceb6698c77c8e64e9c521818cd6bf538517e61a8
Fredrik Nordmoen <nordmoen@stud.ntnu.no>
2012-01-17 18:50:02 +0100

    La til classList.css i main.php et annet sted også…

--- themes/hybrida/views/layouts/main.php
+++ themes/hybrida/views/layouts/main.php
@@ -5,16 +5,17 @@
         <title><?= $this->getPageTitle() ?> - <?= CHtml::encode(Yii::app()->name) ?></title>
         <meta http-equiv="content-type" content="text/html; charset=utf-8" />
         
-        <script type = 'text/javascript' src = '<?= Yii::app()->request->baseUrl ?>/scripts/e.js'></script>
-        <script type = 'text/javascript' src = '<?= Yii::app()->request->baseUrl ?>/scripts/animations.js'></script>
+        <script type="text/javascript" src="<?=Yii::app()->request->baseUrl?>/scripts/e.js"></script>
+        <script type="text/javascript" src="<?=Yii::app()->request->baseUrl?>/scripts/animations.js"></script>
 
-        <link rel="stylesheet" type="text/css" href="<?= Yii::app()->request->baseUrl ?>/style/style.css" />
-        <link rel="stylesheet" type="text/css" href="<?= Yii::app()->request->baseUrl ?>/style/hintList.css" />
-
-        <link rel="stylesheet" type="text/css" href="<?= Yii::app()->request->baseUrl ?>/style/layout.css" />
-        <link rel='stylesheet' type='text/css' href='<?= Yii::app()->request->baseUrl ?>/style/destroy.css'/>
-        <link rel="stylesheet" type="text/css" href="<?= Yii::app()->request->baseUrl ?>/fonts/fonts.css" />
-        <link rel='stylesheet' type='text/css' href="<?= Yii::app()->request->baseUrl ?>/style/pageContent.css" />
+        <link rel="stylesheet" type="text/css" href="<?=Yii::app()->request->baseUrl?>/style/style.css" />
+        <link rel="stylesheet" type="text/css" href="<?=Yii::app()->request->baseUrl?>/style/hintList.css" />
+        <link rel="stylesheet" type="text/css" href="<?=Yii::app()->request->baseUrl?>/style/layout.css" />
+        <link rel="stylesheet" type="text/css" href="<?=Yii::app()->request->baseUrl?>/style/destroy.css" />
+        <link rel="stylesheet" type="text/css" href="<?=Yii::app()->request->baseUrl?>/fonts/fonts.css" />
+        <link rel="stylesheet" type="text/css" href="<?=Yii::app()->request->baseUrl?>/style/pageContent.css" />
+        <link rel="stylesheet" type="text/css" href="<?=Yii::app()->request->baseUrl?>/style/classList.css" />
+        
         <!-- google analytics -->
         <script type="text/javascript">
             var _gaq = _gaq || [];
