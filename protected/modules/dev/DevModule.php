48c7f49f54cba4b5b9f6250588dc690528d789d9
Sigurd Holsen <sighol@gmail.com>
2012-01-17 22:55:46 +0100

    La til sperre på Devmodulen. Devmodulen skal ikke lenger fungere på dev.hybrida.no

--- protected/modules/dev/DevModule.php
+++ protected/modules/dev/DevModule.php
@@ -1,23 +1,29 @@
 <?php
 
-class DevModule extends CWebModule
-{
-	public function init()
-	{
+class DevModule extends CWebModule {
+
+	public function init() {
 		// this method is called when the module is being created
 		// you may place code here to customize the module or the application
-
 		// import the module-level models and components
 		$this->setImport(array(
 			'dev.models.*',
 			'dev.components.*',
 		));
+		$this->throwExceptinIfBadServerName();
 	}
 
-	public function beforeControllerAction($controller, $action)
-	{
-		if(parent::beforeControllerAction($controller, $action))
-		{
+	public function throwExceptinIfBadServerName() {
+		$serverName = Yii::app()->request->serverName;
+		$badServerName = "/hybrida.no/";
+		$isBadServer = preg_match($badServerName, $serverName);
+		if ($isBadServer) {
+			throw new CHttpException('Ingen tilgang', "Denne siden er kun tilgjengelig for webkom-medlemmer");
+		}
+	}
+
+	public function beforeControllerAction($controller, $action) {
+		if (parent::beforeControllerAction($controller, $action)) {
 			// this method is called before any module controller action is performed
 			// you may place customized code here
 			return true;
@@ -25,4 +31,5 @@ class DevModule extends CWebModule
 		else
 			return false;
 	}
+
 }
