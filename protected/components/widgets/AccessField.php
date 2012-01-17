492afd4ba818cff4b0827e8859d9dfda7c131c05
Sigurd Holsen <sighol@gmail.com>
2012-01-17 14:45:06 +0100

    bad: la til mer på accessField

--- protected/components/widgets/AccessField.php
+++ protected/components/widgets/AccessField.php
@@ -4,17 +4,38 @@ class AccessField extends CWidget {
 	
 	public $model;
 	public $attribute;
+	public $attributePath;
 	public $options;
 
 	public function init() {
 		CHtml::resolveNameID($this->model, $this->attribute, $this->options);
-		
 	}
 
 	public function run() {
 		$this->render('accessField', array(
-			
+			'name' => $this->options['name'],
+			'groups' => $this->getGroups(),
 		));
+	}
+	
+	public function getChecked($access) {
+		
+	}
+	
+	public function getName($access) {
+		return "";
+	}
+	
+	public function getGroups() {
+		return array(
+			'Generelt' => array(
+				'Innloggede' => Access::REGISTERED,
+			),
+			'Kjønn' => array(
+				'Mann' => Access::MALE,
+			),
+			'Utgangsår' => $this->getYears(),
+		);
 	}
 	
 }
--- protected/components/widgets/views/accessField.php
+++ protected/components/widgets/views/accessField.php
@@ -1,1 +1,15 @@
-AccessField
+<div>
+	<? foreach ($groups as $groupName => $group): ?>
+		<h3><?= $groupName ?></h3>
+		<div>
+			<? foreach ($group as $accessName => $access): ?>
+				<input 
+					type="checkbox" 
+					value="<?=$access?>" 
+					name="<?= $this->getName($access) ?>" 
+					<?= $this->getChecked($access); ?>
+					/> <?= $accessName ?>
+			<? endforeach ?>
+		</div>
+	<? endforeach ?>
+</div>
