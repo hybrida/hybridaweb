3a098dd603b72e2961f87eef29a2ffc7b57d63a4
Sigurd Holsen <sighol@gmail.com>
2012-01-19 20:10:54 +0100

    La til noen påmeldingsmetoder i Signup

--- protected/models/Signup.php
+++ protected/models/Signup.php
@@ -10,49 +10,45 @@
  * @property string $close
  * @property string $signoff
  */
-class Signup extends CActiveRecord
-{
-	
+class Signup extends CActiveRecord {
+
 	private $_access;
+
 	/**
 	 * Returns the static model of the specified AR class.
 	 * @return Signup the static model class
 	 */
-	public static function model($className=__CLASS__)
-	{
+	public static function model($className = __CLASS__) {
 		return parent::model($className);
 	}
 
 	/**
 	 * @return string the associated database table name
 	 */
-	public function tableName()
-	{
+	public function tableName() {
 		return 'signup';
 	}
 
 	/**
 	 * @return array validation rules for model attributes.
 	 */
-	public function rules()
-	{
+	public function rules() {
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
 			array('eventId, spots, open, close', 'required'),
-			array('eventId, spots', 'numerical', 'integerOnly'=>true),
-			array('signoff', 'length', 'max'=>5),
+			array('eventId, spots', 'numerical', 'integerOnly' => true),
+			array('signoff', 'length', 'max' => 5),
 			// The following rule is used by search().
 			// Please remove those attributes that should not be searched.
-			array('eventId, spots, open, close, signoff', 'safe', 'on'=>'search'),
+			array('eventId, spots, open, close, signoff', 'safe', 'on' => 'search'),
 		);
 	}
 
 	/**
 	 * @return array relational rules.
 	 */
-	public function relations()
-	{
+	public function relations() {
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
 		return array(
@@ -62,8 +58,7 @@ class Signup extends CActiveRecord
 	/**
 	 * @return array customized attribute labels (name=>label)
 	 */
-	public function attributeLabels()
-	{
+	public function attributeLabels() {
 		return array(
 			'eventId' => 'Event',
 			'spots' => 'Spots',
@@ -78,25 +73,24 @@ class Signup extends CActiveRecord
 	 * Retrieves a list of models based on the current search/filter conditions.
 	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
 	 */
-	public function search()
-	{
+	public function search() {
 		// Warning: Please modify the following code to remove attributes that
 		// should not be searched.
 
-		$criteria=new CDbCriteria;
+		$criteria = new CDbCriteria;
 
-		$criteria->compare('eventId',$this->eventId);
-		$criteria->compare('spots',$this->spots);
-		$criteria->compare('open',$this->open,true);
-		$criteria->compare('close',$this->close,true);
-		$criteria->compare('signoff',$this->signoff,true);
-		$criteria->compare('active',$this->active);
+		$criteria->compare('eventId', $this->eventId);
+		$criteria->compare('spots', $this->spots);
+		$criteria->compare('open', $this->open, true);
+		$criteria->compare('close', $this->close, true);
+		$criteria->compare('signoff', $this->signoff, true);
+		$criteria->compare('active', $this->active);
 
 		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
-		));
+					'criteria' => $criteria,
+				));
 	}
-	
+
 	public function afterConstruct() {
 		$this->_access = new AccessRelation($this);
 	}
@@ -116,4 +110,37 @@ class Signup extends CActiveRecord
 	public function afterSave() {
 		$this->_access->replace();
 	}
+
+	public function addAttender($userId) {
+		$stmt = app()->db->createCommand()
+				->insert('membership_signup', array(
+			'eventId' => $this->eventId,
+			'userId' => $userId,
+			'signedOff' => 'false',
+		));
+	}
+	
+	public function removeAttender($userId) {
+		Yii::app()->db->createCommand()
+				->delete('membership_signup', 
+						'eventId = :eventId AND userId = :userId', array(
+					':eventId' => $this->eventId,
+					':userId' => $userId,
+				));
+	}
+
+	public function getAttendingCount() {
+		$sql = "SELECT COUNT(*) as num 
+			FROM membership_signup
+			WHERE eventId = :eventId";
+		$pdo = Yii::app()->db->getPdoInstance();
+		$stmt = Yii::app()->db->getPdoInstance()->prepare($sql);
+
+		$stmt->execute(array(
+			':eventId' => $this->eventId,
+		));
+		$data = $stmt->fetch(PDO::FETCH_ASSOC);
+		return $data['num'];
+	}
+
 }
--- protected/tests/unit/models/SignupTest.php
+++ protected/tests/unit/models/SignupTest.php
@@ -1,25 +1,34 @@
 <?php
 
 class SignupTest extends CTestCase {
-	
+
 	public function getSignupObject() {
 		$signup = new Signup;
 		$signup->spots = 1;
 		$signup->close = "2011.10.22 14:30";
 		$signup->open = "2011.10.22 14:30";
-		$signup->eventId = rand(1000,10000000);
+		$signup->eventId = rand(1000, 10000000);
 		return $signup;
 	}
-	
+
+	public function getUser() {
+		// username, firstName, lastName, member
+		$user = new User;
+		$user->username = 'test' . User::model()->count();
+		$user->firstName = $user->lastName = "test";
+		$user->member = "true";
+		$this->assertTrue($user->save());
+		return $user;
+	}
+
 	public function test_validate() {
 		$signup = $this->getSignupObject();
 		$validate = $signup->validate();
-		if (! $validate) {
-			print_r( $signup->getErrors());
+		if (!$validate) {
+			print_r($signup->getErrors());
 		}
 		$this->assertTrue($signup->validate());
 	}
-
 
 	public function test_insert_() {
 		$signup = $this->getSignupObject();
@@ -65,6 +74,38 @@ class SignupTest extends CTestCase {
 		$this->assertNotEquals(null, $signup->primaryKey);
 	}
 
+	public function test_addAttending_attendingCount_oneAttending() {
+		$signup = $this->getSignupObject();
+		$user = $this->getUser();
+		$signup->addAttender($user->id);
+		$this->assertEquals(1, $signup->attendingCount);
+	}
 
-	/* */
+	public function test_addAttending_attendingCount_fewAttending() {
+		$signup = $this->getSignupObject();
+		$user1 = $this->getUser();
+		$user2 = $this->getUser();
+		$signup->addAttender($user1->id);
+		$signup->addAttender($user2->id);
+		$this->assertEquals(2, $signup->attendingCount);
+	}
+
+	public function test_addAttendig_attendingCount_none() {
+		$signup = $this->getSignupObject();
+		$this->assertEquals(0, $signup->attendingCount);
+	}
+
+	public function test_removeAttending() {
+		$signup = $this->getSignupObject();
+		$user = $this->getUser();
+		$signup->addAttender($user->id);
+		$signup->removeAttender($user->id);
+		$this->assertEquals(0, $signup->attendingCount);
+	}
+
+	public function test_removeAttending_noneExisting() {
+		$signup = $this->getSignupObject();
+		$signup->removeAttender(123139817243);
+	}
+
 }
