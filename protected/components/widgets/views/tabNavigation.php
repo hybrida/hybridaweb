82e990f5a5d01cf5ef303041691fb6db36d15086
Sigurd Holsen <sighol@gmail.com>
2012-02-08 14:29:03 +0100

    Endret på navigasjonslinjen

--- protected/components/widgets/views/tabNavigation.php
+++ protected/components/widgets/views/tabNavigation.php
@@ -1,8 +1,12 @@
 <items>
-	<div><?= CHtml::link("Hjem", array("/news/feed")); ?></div> 
-	<div><?= CHtml::link("Profil", array("/profile/")); ?></div> 
-	<div><?= CHtml::link("Grupper", array("/group/")); ?></div> 
-	<div><?= CHtml::link("Personer", array("/students/")); ?></div> 
+	<div><?= CHtml::link("Forsiden", array("/news/feed")); ?></div> 
 	<div><?= CHtml::link("Hybrida", array("/article/1")); ?></div>
-	<div class="last"><?= CHtml::link("Bedrift", array("/article/2")); ?></div> 
+	<div><?= CHtml::link("Bedrift", array("/article/2")); ?></div> 
+	<? if (!user()->isGuest): ?>
+	<div><?= CHtml::link("Profil", array("/profile/")); ?></div>
+	<div><?= CHtml::link("Personer", array("/students/")); ?></div> 
+	<? endif ?>
+	<? if (app()->gatekeeper->hasGroupAccess(57)): ?>
+	<div><?= CHtml::link("BK", array('/bk/bktool/index')) ?> </div>
+	<? endif ?>
 </items>
