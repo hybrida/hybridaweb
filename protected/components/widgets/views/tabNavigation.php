01ac79e027f4a1da4641ee717997b3826a8842ce
Sigurd Holsen <sighol@gmail.com>
2012-02-08 22:52:15 +0100

    Flyttet litt mer om på toppmenyen

--- protected/components/widgets/views/tabNavigation.php
+++ protected/components/widgets/views/tabNavigation.php
@@ -1,12 +1,15 @@
 <items>
 	<div><?= CHtml::link("Forsiden", array("/news/feed")); ?></div> 
-	<div><?= CHtml::link("Hybrida", array("/article/1")); ?></div>
-	<div><?= CHtml::link("Bedrift", array("/article/2")); ?></div> 
-	<? if (!user()->isGuest): ?>
-	<div><?= CHtml::link("Profil", array("/profile/")); ?></div>
-	<div><?= CHtml::link("Personer", array("/students/")); ?></div> 
-	<? endif ?>
+
 	<? if (app()->gatekeeper->hasGroupAccess(57)): ?>
-	<div><?= CHtml::link("BK", array('/bk/bktool/index')) ?> </div>
+		<div><?= CHtml::link("BK", array('/bk/bktool/index')) ?> </div>
 	<? endif ?>
+
+	<? if (!user()->isGuest): ?>
+		<div><?= CHtml::link("Profil", array("/profile/")); ?></div>
+		<div><?= CHtml::link("Medlemmer", array("/students/")); ?></div> 
+	<? endif ?>
+
+	<div><?= CHtml::link("Bedrift", array("/article/2")); ?></div> 
+	<div><?= CHtml::link("Om Hybrida", array("/article/1")); ?></div>
 </items>
