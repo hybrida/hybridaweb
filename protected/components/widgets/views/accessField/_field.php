9792274abbc9ca46fb33393f29e963ea3c51ba56
Sigurd Holsen <sighol@gmail.com>
2012-02-19 20:34:39 +0100

    tilganger: Ny lukkeknapp på access-felt

--- protected/components/widgets/views/accessField/_field.php
+++ protected/components/widgets/views/accessField/_field.php
@@ -1,15 +1,22 @@
-<? if ($sub != 0): ?>
-	<h2>Eller</h2>
-<? endif ?>
+<div>
+	<br clear="all">
+	<?= CHtml::button('X',array(
+		'class' => 'button buttonRightSide',
+		'onclick' => "js:$(this).closest('div').remove()",
+	)) ?>
+	<? if ($sub != 0): ?>
+		<h2>Eller</h2>
+	<? endif ?>
 
-<? foreach ($this->getAccessGroups() as $groupName => $group): ?>
-	<div class="accessGroup" id="<?= $groupName ?>">
-		<h3><?= $groupName ?></h3>
-		<? foreach ($group as $accessName => $access): ?>
-			<div class="accessItem">
-				<input class="accessInput"	type="checkbox" value="<?= $access ?>" name="<?= $this->getName($access, $sub) ?>" <?= $this->getChecked($access, $sub); ?>	/>
-				<div class="accessName" ><?= $accessName ?></div>
-			</div>
-		<? endforeach ?>
-	</div>
-<? endforeach ?>
+	<? foreach ($this->getAccessGroups() as $groupName => $group): ?>
+		<div class="accessGroup" id="<?= $groupName ?>">
+			<h3><?= $groupName ?></h3>
+			<? foreach ($group as $accessName => $access): ?>
+				<div class="accessItem">
+					<input class="accessInput"	type="checkbox" value="<?= $access ?>" name="<?= $this->getName($access, $sub) ?>" <?= $this->getChecked($access, $sub); ?>	/>
+					<div class="accessName" ><?= $accessName ?></div>
+				</div>
+			<? endforeach ?>
+		</div>
+	<? endforeach ?>
+</div>
