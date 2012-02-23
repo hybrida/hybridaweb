e03518d575f12c337e04f4f4a7743c1faa80cd6e
Sigurd Holsen <sighol@gmail.com>
2012-02-23 10:44:42 +0100

    bugfix: alle datoer ble til mars

--- protected/components/helpers/Html.php
+++ protected/components/helpers/Html.php
@@ -1,25 +1,25 @@
 <?php
 
 class Html extends CHtml {
-	
-	public static function dateToString($dateString, $format='medium') {
-		$longMonthNames = array(
+	private static $longMonthNames = array(
 			'januar','februar','mars','april','mai','juni',
 			'juli','august','september','oktober','november',
 			'desember'
 		);
-		$shortMonthNames = array(
+		private static $shortMonthNames = array(
 			'jan.', 'feb.', "mar.", "apr.","mai","jun.","jul.",
 			"aug.","sep.","okt.", "nov.", "des."
 		);
+	
+	public static function dateToString($dateString, $format='medium') {
 		$date = strtotime($dateString);
-		$month = date('n');
+		$month = date('n', $date);
 		if ($format == 'short')
 			return date('n.m.Y', $date);
 		if ($format == 'medium')
-			return date('j. ', $date) . $shortMonthNames[$month] . date(' Y', $date);
+			return date('j. ', $date) . self::$shortMonthNames[$month - 1] . date(' Y', $date);
 		if ($format == 'long')
-			return date('j. ', $date) . $longMonthNames[$month] . date(' Y H:i', $date);
+			return date('j. ', $date) . self::$longMonthNames[$month - 1] . date(' Y H:i', $date);
 		else
 			return date($format, $date);
 	}
