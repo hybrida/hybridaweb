38e014334809c0f6863bebdfd39b542dfc0e5e38
Sigurd Holsen <sighol@gmail.com>
2012-02-26 14:32:38 +0100

    tabNavigation: Rettet opp i ikke riktig skrevne lenker

--- protected/components/widgets/views/tabNavigation.php
+++ protected/components/widgets/views/tabNavigation.php
@@ -10,6 +10,6 @@
 		<div><?= CHtml::link("Medlemmer", array("/students/")); ?></div> 
 	<? endif ?>
 
-	<div><?= CHtml::link("Bedrift", array("/article/2")); ?></div> 
-	<div><?= CHtml::link("Om Hybrida", array("/article/1")); ?></div>
+	<div><?= CHtml::link("Bedrift", array("/article/view", 'id' => 2)); ?></div> 
+	<div><?= CHtml::link("Om Hybrida", array("/article/view", 'id' => 2)); ?></div>
 </items>
