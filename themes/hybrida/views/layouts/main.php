4b748a7d41a1c0b967b593fb512ea9a15c4c9a1e
Sigurd Holsen <sighol@gmail.com>
2012-03-08 18:46:01 +0100

    Endret griff-lenken fra /news/feed til å gå til /

--- themes/hybrida/views/layouts/main.php
+++ themes/hybrida/views/layouts/main.php
@@ -36,7 +36,7 @@
     <body>
         <div class="headerStripe">
             <div class="header">
-                <a href="/news/feed" ><img src="<?= Yii::app()->request->baseUrl ?>/images/mastHeadLogo.png" align="left" alt="" /></a>
+                <a href="/" ><img src="<?= Yii::app()->request->baseUrl ?>/images/mastHeadLogo.png" align="left" alt="" /></a>
                 <h1><?= CHtml::encode(Yii::app()->name) ?></h1>
                 <div class="searchWrap">
 					<? $this->widget('search.components.SearchWidget') ?>
