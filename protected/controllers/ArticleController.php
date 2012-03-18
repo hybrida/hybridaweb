e80ae761003de9c9989e6121049e24df95d12ca3
Sigurd Holsen <sighol@gmail.com>
2012-03-18 16:07:41 +0100

    Fjernet gammel kode og ryddet litt

--- protected/controllers/ArticleController.php
+++ protected/controllers/ArticleController.php
@@ -1,53 +1,47 @@
 <?php
 
 class ArticleController extends Controller {
-	
-	public function filters() {
-		return array(
-			'accessControl',
-		);
-	}
-	
-    public function actionView($id) {
-        $article = $this->getArticleModelAndThrowExceptionIfNullOrNotAccess($id);
-		
+
+	public function actionView($id) {
+		$article = $this->getArticleModelAndThrowExceptionIfNullOrNotAccess($id);
+
 		$this->render("view", array(
 			'article' => $article,
 			'hasEditAccess' => user()->checkAccess('updateArticle'),
 		));
-    }
-    
-    public function actionCollection(){
+	}
+
+	public function actionCollection() {
 		$article = new Article();
-        $articleList['menuelements'] = $article->listArticles('wiki');      //Litt for statisk
+		$articleList['menuelements'] = $article->listArticles('wiki');	  //Litt for statisk
 
-        $aId = $articleList[0][0];
+		$aId = $articleList[0][0];
 
-        $data = $article->getArticle($aId);
-		
-        $this->renderPartial('menu',$articleList);
-        $this->render('view',$data);
-    }
-	
+		$data = $article->getArticle($aId);
+
+		$this->renderPartial('menu', $articleList);
+		$this->render('view', $data);
+	}
+
 	public function actionCreate($parentId = null) {
-		if(!user()->checkAccess('createArticle')) {
+		if (!user()->checkAccess('createArticle')) {
 			throw new CHttpException(403, "Du har ikke tilgang");
 		}
-		
+
 		$model = new Article;
 		$model->parentId = $parentId;
-		$this->renderArticleForm($model);		
+		$this->renderArticleForm($model);
 	}
-    
-    public function actionEdit($id){
-		if(!user()->checkAccess('updateArticle', array('id' => $id))) {
+
+	public function actionEdit($id) {
+		if (!user()->checkAccess('updateArticle', array('id' => $id))) {
 			throw new CHttpException(403, "Du har ikke tilgang");
 		}
-		
+
 		$model = $this->getArticleModel($id);
 		$this->renderArticleForm($model);
-    }
-	
+	}
+
 	private function getArticleModelAndThrowExceptionIfNullOrNotAccess($id) {
 		$article = Article::model()->findByPk($id);
 
@@ -58,28 +52,27 @@ class ArticleController extends Controller {
 		return $article;
 	}
 
-	
 	private function renderArticleForm($model) {
 		if (isset($_POST['Article'])) {
 			$model->setAttributes($_POST['Article']);
 			$model->purify();
 			$model->save();
-			
+
 			$this->redirect($model->getViewUrl());
 			return;
 		}
-		
-		$this->render('edit',
-				array(
-					'model'=> $model,
-			));
+
+		$this->render('edit', array(
+			'model' => $model,
+		));
 	}
-    
-    public function getArticleModel($id) {
-        $model = Article::model()->findByPk($id);
-        if ($model)
-            return $model;
-        else
-            throw new CHttpException("Artikkelen finnes ikke");
+
+	public function getArticleModel($id) {
+		$model = Article::model()->findByPk($id);
+		if ($model)
+			return $model;
+		else
+			throw new CHttpException("Artikkelen finnes ikke");
 	}
+
 }
--- protected/views/faq/view.php
+++
@@ -1,9 +0,0 @@
-<ul>
-    F.A.Q
-    <? foreach($articles as $article) : ?>
-
-        <li><a href='/yii/article/<?= $article['id'] ?>'><?= $article['title']; ?> </a></li>
-
-    <? endforeach; ?>
-
-</ul>
