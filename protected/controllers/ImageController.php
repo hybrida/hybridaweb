0175004cd171c2701bf63b9cafb5e6bd1783b99c
Sigurd Holsen <sighol@gmail.com>
2012-04-06 00:36:04 +0200

    refaktorerte ImageController

--- protected/controllers/ImageController.php
+++ protected/controllers/ImageController.php
@@ -1,23 +1,6 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of ImageController
- *
- * @author sigurd
- */
 class ImageController extends Controller {
-
-	public $pdo;
-	private $bigThumb;
-	private $smallThumb;
-	private $microThumb;
-	private $upcPath;
-	private $imagePath;
 
 	private function renderImage($filename) {
 		ob_clean();
