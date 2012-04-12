988017ae9ffc98a4dc8be6abf74f6d94f71bb623
Sigurd Holsen <sighol@gmail.com>
2012-04-12 23:47:49 +0200

    bugfix: "ELLER" ble vist uansett foran alle accessField og ikke bare mellom.

--- protected/components/widgets/views/accessField/_field.php
+++ protected/components/widgets/views/accessField/_field.php
@@ -3,7 +3,9 @@
 		'class' => 'button buttonRightSide',
 		'onclick' => "js:$(this).closest('div').remove()",
 	)) ?>
+	<? if ($sub != 0): ?>
 	<h2>Eller</h2>
+	<? endif ?>
 
 	<? foreach ($this->getAccessGroups() as $groupName => $group): ?>
 		<div class="accessGroup" id="<?= $groupName ?>">
