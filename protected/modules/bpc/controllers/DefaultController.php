e1cabde271f1856cd2d6d3efce7c082a1fe0374d
Sigurd Holsen <sighol@gmail.com>
2012-04-14 17:07:19 +0200

    bugfix: bpc: Kun lov å melde seg på med egen brukerid

--- protected/modules/bpc/controllers/DefaultController.php
+++ protected/modules/bpc/controllers/DefaultController.php
@@ -13,6 +13,9 @@ class DefaultController extends Controller {
 	}
 
 	public function actionToggleAttending($userId, $bpcId) {
+		if ($userId != user()->id) {
+			throw new CHttpException(403, "Du har ikke tilgang til å endre andre brukeres påmelding");
+		}
 		$event = new BpcEvent($bpcId);
 		if ($event->canAttend($userId)) {
 			if (!$event->isAttending($userId)) {
