448accf9844c278cbf275723edff81eb7aeba020
Sigurd Holsen <sighol@gmail.com>
2012-04-24 12:00:40 +0200

    Stilsatte hoyremargen

--- protected/views/news/_view_sidebar.php
+++ protected/views/news/_view_sidebar.php
@@ -3,16 +3,16 @@
 		<div class="barTitle">Event</div>
 		<div class="barText">
 			<b>Sted: </b><?= $event->location ?> <br>
-			<b>Starter: </b><i><?= Html::dateToString($event->start, 'long') ?></i><br />
-			<b>Slutter: </b><i><?= Html::dateToString($event->end, 'long') ?></i>
+			<b>Fra: </b><i><?= Html::dateToString($event->start, 'long') ?></i><br />
+			<b>Til: </b><i><?= Html::dateToString($event->end, 'long') ?></i>
 		</div>
 
 		<? if ($signup): ?>
 			<div class="barTitle">Påmelding:</div>
 			<div class="barText">
 				<strong>Påmeldte: </strong> <i><?= $signup->attendingCount ?> av <?= $signup->spots ?></i> <br/>
-				<strong>Åpner: </strong><i><?= Html::dateToString($signup->open, 'long') ?></i> <br>
-				<strong>Stenger: </strong><i><?= Html::dateToString($signup->close, 'long') ?></i> <br>
+				<strong>Fra: </strong><i><?= Html::dateToString($signup->open, 'long') ?></i> <br>
+				<strong>Til: </strong><i><?= Html::dateToString($signup->close, 'long') ?></i> <br>
 			</div>
 		<? endif ?>
 		<div class="barTitle">Google calendar</div>
