340981f09c7e611c36015614a8df7ca9b22b3dd2
Fredrik Nordmoen <nordmoen@stud.ntnu.no>
2012-04-26 03:01:08 +0200

    Dynamisk sidebar med js, forhåpentligvis

---
+++ scripts/sidebar.js
@@ -0,0 +1,26 @@
+var sidebarWidth = 230;
+var pageMin = 800;
+var pageMax = 1200;
+
+$('window').resize(function () {
+	setSidebarHeight();
+	setSidebarPos();
+)};
+
+function setSidebarHeight() {
+	$('document').getElementById("sidebar").style[height]=$('document').documentElement.innerHeight-90+"px";
+
+function setSidebarPos(){
+	var leftValue = 0;
+	var width = $('document').documentElement.innerHeight;
+	if (width < pageMax) {
+		leftValue = pageMin - sidebarWidth;
+	}
+	else if (width > pageMax) {
+		leftValue = (width-pageMax)/2+pageMax-sidebarWidth;
+	}
+	else {
+		width-sidebarWidth;
+	}
+	$('document').getElementById("sidebar").style[left]=leftvalue+"px";
+};
--- themes/hybrida/views/layouts/main.php
+++ themes/hybrida/views/layouts/main.php
@@ -9,6 +9,7 @@
         <script type="text/javascript" src="<?=Yii::app()->request->baseUrl?>/scripts/animations.js"></script>
 	<script type = 'text/javascript' src = '<?=Yii::app()->request->baseUrl?>/scripts/CollapsibleLists.js'></script>
 	<script type = 'text/javascript' src = '<?=Yii::app()->request->baseUrl?>/scripts/OnloadScheduler.js'></script>
+	<script type="text/javascript" src = '<?=Yii::app()->request->baseUrl?>/scripts/sidebar.js'></script>
 	
 	<script type="text/javascript">
 		OnloadScheduler.schedule(function(){ CollapsibleLists.apply(); });
