a42e666ea32e4e3a3428d4b1ca12ac5e04c1405e
Sigurd Holsen <sighol@gmail.com>
2012-08-25 19:48:58 +0200

    La til I&IKT-ringen på hovedmenyen

--- protected/components/widgets/views/tabNavigation.php
+++ protected/components/widgets/views/tabNavigation.php
@@ -13,5 +13,6 @@
 	<? endif ?>
 
 	<div><?= CHtml::link("Bedrift", array("/article/view", 'id' => 2, 'title' => 'Bedrift')); ?></div> 
+	<div><?= CHtml::link("I&IKT-ringen", array("/article/view", 'id' => 62, 'title' => 'IKT-ringen')); ?></div>
 	<div><?= CHtml::link("Om Hybrida", array("/article/view", 'id' => 1, 'title' => 'Om Hybrida')); ?></div>
 </div>
