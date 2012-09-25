3cbf678abb0a9ec2413f4d4ed2ab3dbf5c51c815
Sigurd Holsen <sighol@gmail.com>
2012-09-26 00:01:51 +0200

    design: Floater login/logut-knapp til høyre på menyen

--- protected/components/widgets/views/tabNavigation.php
+++ protected/components/widgets/views/tabNavigation.php
@@ -1,33 +1,27 @@
 <div class="navItems">
-	<div><?= CHtml::link("Kalender", array("/calendar/default/index")); ?></div> 
+	<?= CHtml::link("Kalender", array("/calendar/default/index")); ?> 
 
 	<? if (app()->gatekeeper->hasGroupAccess(57)): ?>
-		<div><?= CHtml::link("BK", array('/bk/bktool/index')) ?> </div>
+		<?= CHtml::link("BK", array('/bk/bktool/index')) ?> 
 	<? endif ?>
 
 	<? if (!user()->isGuest): ?>
-		<div><?= CHtml::link("Profil", array("/profile/")); ?></div>
-		<div><?= CHtml::link("Medlemmer", array("/students/")); ?></div> 
-		<div><?= CHtml::link("Kilt", array("/kilt/shop/index")); ?></div> 
+		<?= CHtml::link("Profil", array("/profile/")); ?>
+		<?= CHtml::link("Medlemmer", array("/students/")); ?> 
+		<?= CHtml::link("Kilt", array("/kilt/shop/index")); ?> 
 	<? endif ?>
 
-	<div><?= CHtml::link("Bedrift", array("/article/view", 'id' => 2, 'title' => 'Bedrift')); ?></div> 
-	<div><?= CHtml::link("I&IKT-ringen", array("/article/view", 'id' => 62, 'title' => 'IKT-ringen')); ?></div>
-	<div><?= CHtml::link("Om Hybrida", array("/article/view", 'id' => 1, 'title' => 'Om Hybrida')); ?></div>
+	<?= CHtml::link("Bedrift", array("/article/view", 'id' => 2, 'title' => 'Bedrift')); ?> 
+	<?= CHtml::link("I&IKT-ringen", array("/article/view", 'id' => 62, 'title' => 'IKT-ringen')); ?>
+	<?= CHtml::link("Om Hybrida", array("/article/view", 'id' => 1, 'title' => 'Om Hybrida')); ?>
 	<? Yii::import('notifications.models.*') ?>
 
-	<? if (user()->isGuest): ?>
-		<div><?= CHtml::link("Logg inn", user()->loginUrl) ?></div>
-	<? else: ?>
-		<div class="widget-notification">
-			<? $url = app()->createUrl('notifications/default/index') ?>
-			<a href="<?= $url ?>">
-				<?= count(Notifications::getUnread(user()->id)) ?>
-			</a>
-		</div>
-		<div>
-			<?= CHtml::link(user()->fullName, param('logoutUrl'));
-			?>
-		</div>
-	<? endif ?>
+	<div class="userOptions">
+		<? if (user()->isGuest): ?>
+			<?= CHtml::link("Logg inn", user()->loginUrl) ?>
+		<? else: ?>
+			<?= CHtml::link(user()->fullName, param('logoutUrl')); ?>
+		<? endif ?>
+	</div>
+
 </div>
