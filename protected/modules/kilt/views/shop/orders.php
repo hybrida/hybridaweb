2f64a02ee87d51f0555bb8221012d6a62d89cf94
Håkon Bråten <haakobra@stud.ntnu.no>
2012-09-28 15:27:49 +0200

    kilt: ryddet i orders.php slik at den i større grad følger retningslinjene

--- protected/modules/kilt/views/shop/orders.php
+++ protected/modules/kilt/views/shop/orders.php
@@ -1,83 +1,78 @@
-<?  $this->renderPartial("_menu"); ?>
+<? $this->renderPartial("_menu"); ?>
+<? if (count($timeOrders) == 0 && count($timeComments) == 0): ?>
+	<?= "<p class=\"orderText\">Du har ingen bestillinger</p>"; ?>
+	<? return; ?>
+<? endif; ?>
 <br>
-<?
-if (count($timeOrders) == 0 && count($timeComments) == 0)
-	echo "<p class=\"orderText\">Du har ingen bestillinger</p>"; 
-else
-{
-	echo CHtml::beginForm('/kilt/shop/orders', 'post');
-	foreach($times as $t):
-	   $time_id = $t['id'];
-	   $curr = $time_id == $time['id']; ?>
-		<table class="orderTable">
+<?= CHtml::beginForm('/kilt/shop/orders', 'post'); ?>
+<? foreach($times as $t): ?>
+	<? $time_id = $t['id']; ?>
+	<? $curr = $time_id == $time['id']; ?>
+	<table class="orderTable">
 			<tr>
-				<td colspan=4 class="orderTableTitle">
-					<?
-						if ($curr)
-						   echo "Dine aktive bestillinger:";
-						else
-						{
-							echo "Dine bestillinger i tidsrommet ";
-							echo $times[$time_id]['start'];
-							echo " - ";
-							echo $times[$time_id]['end'];
-						}
-					?>
-					<hr>
-				</td>
-			<tr>
-				<td class="orderTitle">Produkt</td>
-				<td class="orderTitle">Størrelse</td>
-				<td class="orderTitle">Antall</td>
-				<td class="orderTitle"><? if ($curr) echo "Slett"; else echo "Hentet"; ?> </td>
-			</tr>
-	   <? if (!isset($timeOrders[$time_id])) goto comments; ?>
-			<?  foreach($timeOrders[$time_id] as $o): ?>
-			   <tr class="<? if (!$curr) echo ($o['confirmed']) ? "green" : "red"; ?>">
-				   <td class="orderContent">
-					   <?
-					   $id = $o['product_id'];
-					   echo $products[$id]['type'] . ": " . $products[$id]['model'];
-					   ?>
-				   </td>
-				   <td class="orderContent"> <? echo $sizes[$o['product_size']]; ?> </td>
-				   <td class="orderContent"> <? echo $o['product_quantity']; ?> </td>
-				   <td class="orderContent">
-					   <? if ($curr) 
-							   echo CHtml::submitButton('Fjern produkt', 
-							   array( 'name' => $o['id'], 'disabled' => !$curr,));
-						   else
-							   echo ($o['confirmed']) ? "Ja" : "Nei"; ?>
-				   </td>
-			   </tr>
-			   <? endforeach; ?>
-	   <? comments:
-		if (!isset($timeComments[$time_id]))
-			continue;
-		 ?>
-			   <tr>
-				   <td colspan=4>
-					   <hr>
-				   </td>
-			   <tr>
-			   <tr>
-				   <td><b>Info</b></td>
-				   <td colspan=2>
-					  <? echo $timeComments[$time_id]; ?>
-				   </td>
-				   <td>
-					   <? if ($curr) 
-							   echo CHtml::submitButton('Fjern info', 
-							   array( 'name' => $time_id, 'disabled' => !$curr,));
-					   ?>
-				   </td>
-			   <tr>
-		   </table> 
-	<? endforeach;?>
-<?  echo CHtml::endForm(); ?>
-<?  if ($curr) echo "<p class=\"orderText\">" .
-					"Du kan endre din bestilling frem til " . 
-					$time['end'] .
-					"</p>";
-?>
-<? } ?>
+			<td colspan=4 class="orderTableTitle">
+				<?	if ($curr): ?>
+					<?= "Dine aktive bestillinger:"; ?>
+				<? else: ?>
+					<?= "Dine bestillinger i tidsrommet "; ?>
+					<?= $times[$time_id]['start'] . " - " . $times[$time_id]['end'];?>
+				<? endif; ?>
+				<hr>
+			</td>
+		</tr>
+		<tr>
+			<td class="orderTitle">Produkt</td>
+			<td class="orderTitle">Størrelse</td>
+			<td class="orderTitle">Antall</td>
+			<td class="orderTitle">
+				<?= ($curr) ? "Slett" : "Hentet"; ?>
+			</td>
+		</tr>
+		<? if (!isset($timeOrders[$time_id])) goto comments; ?>
+		<? foreach($timeOrders[$time_id] as $o): ?>
+		<tr class="<? if (!$curr): ?> <?= ($o['confirmed']) ? "green" : "red"; ?> <? endif; ?>">
+			<td class="orderContent">
+				<?	$id = $o['product_id']; ?>
+				<?= $products[$id]['type'] . ": " . $products[$id]['model'];?>
+			</td>
+			<td class="orderContent"> 
+				<?= $sizes[$o['product_size']]; ?>
+			</td>
+			<td class="orderContent"> 
+				<?= $o['product_quantity']; ?>
+			</td>
+			<td class="orderContent">
+			<? if ($curr): ?>
+				<?= CHtml::submitButton('Fjern produkt', array( 'name' => $o['id'], 'disabled' => !$curr,));?>
+			<? else: ?>
+				<?= ($o['confirmed']) ? "Ja" : "Nei"; ?>
+			<? endif; ?>
+			</td>
+		</tr>
+		<? endforeach; ?>
+		<? comments: ?>
+		<? if (!isset($timeComments[$time_id])) continue; ?>
+		<tr>
+			<td colspan=4>
+				<hr>
+			</td>
+		</tr>
+		<tr>
+			<td><b>Info</b></td>
+			<td colspan=2>
+				<?= $timeComments[$time_id]; ?>
+			</td>
+			<td>
+			<? if ($curr): ?>
+				<?= CHtml::submitButton('Fjern info', array( 'name' => $time_id, 'disabled' => !$curr,));?>
+			<? endif; ?>
+			</td>
+		<tr>
+	</table> 
+<? endforeach;?>
+<? echo CHtml::endForm(); ?>
+<? if ($curr): ?> 
+	<p class="orderText">
+		<?= "Du kan endre din bestilling frem til " .  $time['end']; ?>
+	</p>
+<? endif; ?>
