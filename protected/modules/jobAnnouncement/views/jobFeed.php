f20a16366a69b1020bcdd10b10e1c547373723fb
Sigurd Holsen <sighol@gmail.com>
2012-10-02 08:14:57 +0200

    Bruker g-sidebarNav på jobAnnouncement-widget

--- protected/modules/jobAnnouncement/views/jobFeed.php
+++ protected/modules/jobAnnouncement/views/jobFeed.php
@@ -1,12 +1,14 @@
 
 <div class='g-barTitle'><?= Html::link("Utlyste jobber", array("/jobAnnouncement/jobAnnouncement/index")) ?></div>
 
-<div class="g-barText">
+<div class="g-sidebarNav">
+	<ul>
 	<? foreach ($models as $model): ?>
-		<?= CHtml::link($model->title, $model->viewUrl) ?> <br>
+		<li><?= CHtml::link($model->title, $model->viewUrl) ?></li>
 	<? endforeach ?>
 
 	<? if (empty($models)): ?>
-		Det er ingen utlyste jobber
+		<li>Det er ingen utlyste jobber</li>
 	<? endif ?>
+	</ul>
 </div>
