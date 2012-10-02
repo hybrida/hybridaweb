f6c4e65ad2b22970e4d2556f27ffe047e2ee6b17
Sigurd Holsen <sighol@gmail.com>
2012-10-02 23:10:26 +0200

    meny-endring: Logg ut istedenfor navn, la til 'Hjem', fjernet 'kilt'

--- protected/components/widgets/views/tabNavigation.php
+++ protected/components/widgets/views/tabNavigation.php
@@ -1,4 +1,5 @@
 <div class="navItems">
+	<?= CHtml::link("Hjem", array("/newsfeed/index")); ?> 
 	<?= CHtml::link("Kalender", array("/calendar/default/index")); ?> 
 
 	<? if (app()->gatekeeper->hasGroupAccess(57)): ?>
@@ -6,21 +7,20 @@
 	<? endif ?>
 
 	<? if (!user()->isGuest): ?>
-		<?= CHtml::link("Profil", array("/profile/")); ?>
+		<?= CHtml::link("Profil", array("/profile/")); ?> 
 		<?= CHtml::link("Medlemmer", array("/students/")); ?> 
-		<?= CHtml::link("Kilt", array("/kilt/shop/index")); ?> 
 	<? endif ?>
 
 	<?= CHtml::link("Bedrift", array("/article/view", 'id' => 2, 'title' => 'Bedrift')); ?> 
-	<?= CHtml::link("I&IKT-ringen", array("/article/view", 'id' => 62, 'title' => 'IKT-ringen')); ?>
-	<?= CHtml::link("Om Hybrida", array("/article/view", 'id' => 1, 'title' => 'Om Hybrida')); ?>
+	<?= CHtml::link("I&IKT-ringen", array("/article/view", 'id' => 62, 'title' => 'IKT-ringen')); ?> 
+	<?= CHtml::link("Om Hybrida", array("/article/view", 'id' => 1, 'title' => 'Om Hybrida')); ?> 
 	<? Yii::import('notifications.models.*') ?>
 
 	<div class="userOptions">
 		<? if (user()->isGuest): ?>
 			<?= CHtml::link("Logg inn", user()->loginUrl) ?>
 		<? else: ?>
-			<?= CHtml::link(user()->fullName, param('logoutUrl')); ?>
+			<?= CHtml::link("Logg ut", param('logoutUrl')); ?>
 		<? endif ?>
 	</div>
 
