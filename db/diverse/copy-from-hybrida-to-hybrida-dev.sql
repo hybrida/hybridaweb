90dfd7f3dfef698dfb55b9b8d685bbaa9723c4cb
Sigurd Holsen <sighol@gmail.com>
2012-10-16 14:38:05 +0200

    db: Endret på skriptet for å overføre fra hybrida til hybrida_dev

--- db/diverse/copy-from-hybrida-to-hybrida-dev.sql
+++ db/diverse/copy-from-hybrida-to-hybrida-dev.sql
@@ -1,5 +1,6 @@
 -- kopierer fra hybrida-database til hybrida_dev
 
+-- Rettigheter
 DELETE FROM hybrida_dev.`rbac_assignment`;
 DELETE FROM hybrida_dev.rbac_item;
 DELETE FROM hybrida_dev.rbac_itemchild;
@@ -8,8 +9,14 @@ INSERT INTO hybrida_dev.rbac_item  SELECT * FROM hybrida.rbac_item;
 INSERT INTO hybrida_dev.rbac_itemchild SELECT * FROM hybrida.rbac_itemchild;
 INSERT INTO hybrida_dev.rbac_assignment SELECT * FROM hybrida.rbac_assignment;
 
+-- Gruppemedlemskap
 DELETE FROM hybrida_dev.group_membership;
 INSERT INTO hybrida_dev.group_membership SELECT * FROM hybrida.group_membership;
 
+-- Artikler
+DELETE FROM hybrida_dev.article;
+INSERT INTO hybrida_dev.article SELECT * FROM hybrida.article;
+
+-- Brukere
 DELETE FROM hybrida_dev.user;
 INSERT INTO hybrida_dev.user SELECT * FROM hybrida.user;
