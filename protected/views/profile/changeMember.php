e4631f08f1a745a4c8420eebaf5432596714da34
Sigurd Holsen <sighol@gmail.com>
2012-10-18 14:50:25 +0200

    changeMember: Ser nå om medlemmet er meldem eller ikke

--- protected/views/profile/changeMember.php
+++ protected/views/profile/changeMember.php
@@ -21,31 +21,42 @@
 		.notmember {
 			background-color: red;
 		}
+		
+		span.member, span.notmember {
+			font-size: 20px;
+		}
 	</style>
 
-	<?if ($user):?>
-	<h1>Endre medlemsskap</h1>
-	<form method="post">
-		<input type="hidden" value="<?= $user->id ?>" />
-		<div class="name">
-			<strong>Navn: </strong> <?= $user->fullName ?> <br/>
-			<strong>Trinn: </strong> <?= $user->classYear ?>. Klasse <br/>
-		</div>
-		<div class="message">
-			<?= $message?>
-		</div>
-		<input class="member" type="submit" name="isMember" value="Medlem"/>
-		<input class="notmember" type="submit" name="isNotMember" value="Ikke Medlem"/>
-	</form>
-	<? endif?>
-	
+	<? if ($user): ?>
+		<h1>Endre medlemsskap</h1>
+		<form method="post">
+			<input type="hidden" value="<?= $user->id ?>" />
+			<div class="name">
+				<strong>Navn: </strong> <?= $user->fullName ?> <br/>
+				<strong>Trinn: </strong> <?= $user->classYear ?>. Klasse <br/>
+				<strong>Medlem: </strong>
+				<? if ($user->member == 'true'): ?>
+					<span class="member">Medlem</span>
+				<? else: ?>
+					<span class="notmember">Ikke Medlem</span>
+				<? endif ?>
+			</div>
+			<div class="message">
+				<?= $message ?>
+			</div>
+			<br/>
+			<input class="member" type="submit" name="isMember" value="Medlem"/>
+			<input class="notmember" type="submit" name="isNotMember" value="Ikke Medlem"/>
+		</form>
+	<? endif ?>
+
 	<h1>Søk</h1>
 	<form method="get">
 		<input type="text" placeholder="Fornavn" name="firstName"/>
 		<input type="text" placeholder="Etternavn" name="lastName" />
 		<input type="submit"/>
 	</form>
-	
+
 	<br/>
 	<br/>
 	<br/>
