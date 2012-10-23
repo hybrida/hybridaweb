446a7bcaf5f2c9e4534a47fb262768ff4d37bb01
Erling Singstad Paulsen <erlingsp@stud.ntnu.no>
2012-10-23 19:12:58 +0200

    updatek: Laget ferdig skript for visning av updatek-utgaver.

--- files/article/updatek.php
+++ files/article/updatek.php
@@ -5,7 +5,7 @@ function getDirContents($folderPath) {
 	$file = readdir($dirHandle);
 	$files = array();
 	while ($file) {
-		if ($file != "." && $file != ".." && $file != ".hg") {
+		if ($file != "." && $file != ".." && $file != ".hg" && $file != ".hgignore") {
 			$files[] = $file;
 		}
 		$file = readdir($dirHandle);
@@ -13,109 +13,92 @@ function getDirContents($folderPath) {
 	return $files;
 }
 
-function referatFolderComparator($folder1, $folder2) {
-	$yearDiff = $folder2->year - $folder1->year;
-	if ($yearDiff == 0) {
-		if ($folder2->season == 'Høst') {
-			return 1;
-		}
-		return -1;
+function getPdfFilesInDirectory($folderPath) {
+    $files = getDirContents($folderPath);
+    $pdfs = array();
+    foreach ($files as $file) {
+        $explode = explode(".", $file);
+        if ($explode[1] == "pdf") {
+            $pdfs[] = $file;
+        }
+    }
+    return $pdfs;
+}
+
+function getUtgaveFolders($folderPath) {
+	$folders = getDirContents($folderPath);
+	$utgaveFolders = array();
+	foreach ($folders as $r) {
+		$utgaveFolders[] = new UtgaveFolder($folderPath . $r);
 	}
+	usort($utgaveFolders, "utgaveFolderComparator");
+	return $utgaveFolders;
+}
+
+function utgaveFolderComparator($folder1, $folder2) {
+	$yearDiff = $folder2->year2 - $folder1->year2;
 	return $yearDiff;
 }
 
-function referatComparator($ref1, $ref2) {
-	return $ref1->time - $ref2->time;
+function utgaveComparator($ref1, $ref2) {
+	return $ref1->nummer - $ref2->nummer;
 }
 
-class ReferatFolder {
-
-	public $referater;
-	public $year;
-	public $season;
-	public $path;
-	public $yearSeason;
-
-	public function __construct($path) {
-		$this->path = $path;
-		$this->setDate($path);
-		$this->setReferater($path);
-		$this->sortReferater();
-	}
-
-	private function setDate($path) {
-		$date = explode("/", $path);
-		$this->yearSeason = $date[count($date) - 1];
-		$this->year = substr($this->yearSeason, 0, 4);
-		$this->season = substr($this->yearSeason, 4) == "v" ? "Vår" : "Høst";
-	}
-
-	private function setReferater($path) {
-		$referater = getDirContents($path);
-		foreach ($referater as $ref) {
-			$this->referater[] = new Referat($ref);
+class UtgaveFolder {
+    
+    public $utgaver;
+    public $year;
+    public $schoolYear;
+    public $path;
+    
+    public function __construct($path) {
+        $this->path = $path;
+        $this->setSchoolYear($path);
+        $this->setUtgaver($path);
+        $this->sortUtgaver();
+    }
+    
+    private function setSchoolYear($path) {
+        $date = explode("/", $path);
+        $this->year1 = substr($date[count($date) - 1], 0, 4);
+        $this->year2 = substr($date[count($date) - 1], 5, 4);
+        $this->schoolYear = $this->year1 . "/" . $this->year2;
+    }
+    
+    private function setUtgaver($path) {
+        $utgaver = getPdfFilesInDirectory($path);
+		foreach ($utgaver as $utg) {
+			$this->utgaver[] = new Utgave($utg);
 		}
-	}
-
-	private function sortReferater() {
-		usort($this->referater, "referatComparator");
-	}
-
+    }
+    
+    private function sortUtgaver() {
+        usort($this->utgaver, "utgaveComparator");
+    }
 }
 
-class Referat {
-
-	public $fileName;
-	public $date;
-	public $time;
-	public $extra;
-
-	public function __construct($fileName) {
-		$this->fileName = $fileName;
-		$this->setDate($fileName);
-	}
-
-	private function setDate($fileName) {
-		$explode = explode(".", $fileName);
-		$dateString = $explode[0];
-		$ex = explode("-", $dateString);
-		if (count($ex) < 3) {
-			debug($ex, "Invalid Filename: " . $fileName);
-			$this->time = 0;
-		} else {
-			$this->time = mktime(0, 0, 0, $ex[1], $ex[2], $ex[0]);
-		}
-		if (isset($ex[3])) {
-			$this->extra = $ex[3];
-		}
-		$date = date('Y-m-d H:i:s', $this->time);
-		$this->date = Html::dateToString($date);
-	}
-
+class Utgave {
+    
+    public $fileName;
+    public $pictureFileName;
+    public $nummer;
+    public $sommer;
+    
+    public function __construct($fileName) {
+        $this->fileName = $fileName;
+        $explode = explode(".", $fileName);
+        $this->pictureFileName = $explode[0] . ".png";
+        $explode2 = explode("-", $explode[0]);
+        $this->nummer = $explode2[1] == "00" ? $this->nummer = "Sommer" : $this->nummer = $explode2[1];
+        
+    }  
 }
 
-function getReferatFolders($folderPath) {
-	$folders = getDirContents($folderPath);
-	$referatFolders = array();
-	foreach ($folders as $r) {
-        debug("", $r);
-		$referatFolders[] = new ReferatFolder($folderPath . $r);
-	}
-	usort($referatFolders, "referatFolderComparator");
-	return $referatFolders;
-}
 
 $folderPath = Yii::getPathOfAlias("webroot") . "/upc/files/updatek/";
 $folderUrl = "/upc/files/updatek/";
 
-$referatMapper = getReferatFolders($folderPath);
-
-
-echo "<pre>";
-print_r (getDirContents($folderPath));
-
-
-die();
+$utgaveMapper = getUtgaveFolders($folderPath);
 ?>
 
 <style>
@@ -123,52 +106,51 @@ die();
 		overflow: auto;
 
 	}
-	.referat a {
+	.utgave a {
 		width: 200px;
 		float: left;
 		padding: 0.5em;
 		margin: 0.5em;
 		background-color: #eee;
 		overflow: auto;
-		border-radius: 0.5em;
+		border-radius: 2px;
 
 	}
 
-	.referat a img {
-		width: 20px;
-		height: 20px;
+	.utgave a img {
+		width: 200px;
+		height: 283px;
 		position: relative;
 		top: 2px;
 	}
 
-	.referat a {
+	.utgave a {
 		color: #000;
 		font-size: 18px;
+        font-weight: bold;
+        text-align: center;
 		text-decoration: none;
 	}
 
-	.referat a:hover {
+	.utgave a:hover {
 		background-color: #D9D9D9;
 	}
 </style>
 
 <div>
-	<?= $article->content ?>
+	<?= $this->content ?>
 </div>
 
 
-<? foreach ($referatMapper as $mappe): ?>
+<? foreach ($utgaveMapper as $mappe): ?>
 	<div class="mappe">
-		<h2><?= $mappe->year . " " . $mappe->season ?></h2>
+		<h2><?= "Skoleåret " . $mappe->schoolYear ?></h2>
 
-		<? foreach ($mappe->referater as $referat): ?>
-			<div class="referat">
-				<a href="<?= $folderUrl ?><?= $mappe->yearSeason ?>/<?= $referat->fileName ?>">
-					<img src="/images/pdf-icon.png" />	
-					<?= $referat->date ?>
-					<div class="extra">
-						<?= $referat->extra ?>
-					</div>
+		<? foreach ($mappe->utgaver as $utgave): ?>
+			<div class="utgave">
+				<a href="<?= $folderUrl ?><?= $mappe->year1 . "-" . $mappe->year2 ?>/<?= $utgave->fileName ?>">
+					<img src="<?= $folderUrl ?><?= $mappe->year1 . "-" . $mappe->year2 ?>/<?= $utgave->pictureFileName ?>" />	
+					<?= $utgave->nummer ?>
 				</a>
 			</div>
 		<? endforeach ?>
