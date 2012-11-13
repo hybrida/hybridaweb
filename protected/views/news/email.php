8cd4cc54042306490767b5475319ca5fce7cf083
Kristian Våge <krisvaage@gmail.com>
2012-11-13 18:05:27 +0100

    nyheter, email: Skiller adressene med komma, istedenfor semikolon (Mail.app taklet hvertfall ikke semikolon)

--- protected/views/news/email.php
+++ protected/views/news/email.php
@@ -2,7 +2,7 @@
 
 	<h1>Epostadresser til de påmeldte på: <?= $news->title ?></h1>
 	<textarea class="emails"><? foreach ($attenders as $attender) {
-			echo $attender->studmail . "; ";
+			echo $attender->studmail . ", ";
 		} ?></textarea>
 
 </div>
