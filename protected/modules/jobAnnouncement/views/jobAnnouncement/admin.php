ffef0b976494e1988f9e568eea45ad8b47f9f54d
Sigurd Holsen <sighol@gmail.com>
2013-01-29 20:06:27 +0100

    jobAnnouncement: Fikset admin-siden og gjorde den litt penere

--- protected/modules/jobAnnouncement/views/jobAnnouncement/admin.php
+++ protected/modules/jobAnnouncement/views/jobAnnouncement/admin.php
@@ -39,11 +39,9 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 	'dataProvider'=>$model->search(),
 	'filter'=>$model,
 	'columns'=>array(
-		'id',
-		'companyId',
-		'deadline',
 		'title',
-		'description',
+		'company.companyName',
+		'deadline',
 		array(
 			'class'=>'CButtonColumn',
 		),
