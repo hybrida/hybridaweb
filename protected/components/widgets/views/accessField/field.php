e4f9747b0f5c526eb30811566cf686d6da37edc7
Erling Singstad Paulsen <erlingsp@stud.ntnu.no>
2013-02-05 19:58:19 +0100

    Endringer i tilgangsfelt-oppsett.

--- protected/components/widgets/views/accessField/field.php
+++ protected/components/widgets/views/accessField/field.php
@@ -1,41 +1,41 @@
 <div class="accessWrapper<?=$this->id?>">
-<div class="accessField unique<?=$this->id?>">
+    <div class="accessField unique<?=$this->id?>">
 
-<? for ($sub = 0; $sub < $subs; $sub++): ?>
-	<?php
-		$this->render('accessField/_field', array(
-			'sub' => $sub,
-		));
-	?>
-<? endfor ?>
+    <? for ($sub = 0; $sub < $subs; $sub++): ?>
+        <?php
+            $this->render('accessField/_field', array(
+                'sub' => $sub,
+            ));
+        ?>
+    <? endfor ?>
 
-</div>
+    </div>
 
-<?=
-CHtml::button('Legg til nytt tilgangsfelt', array(
-	'class' => "g-button fetchNewAccessBlock{$this->id}",
-))
-?>
+    <?=
+    CHtml::button('Legg til nytt tilgangsfelt', array(
+        'class' => "g-button fetchNewAccessBlock{$this->id}",
+    ))
+    ?>
 
-<script type="text/javascript">
-	var sub = <?=$this->sub?>;
-	$(".fetchNewAccessBlock<?=$this->id?>").click(function(){
-		
-		$.ajax({
-			success: function(html){
-				$(".unique<?=$this->id?>").append(html);
-				sub ++;
-			},
-			type: 'get',
-			url: '<?=app()->createUrl('ajax/get/getAccessBlock', array(
-				'name' => $this->name,
-				'id' => $this->id,
-				'sub' => '',
-				))?>' + sub,
-			cache: false,
-			dataType: 'html'
-		});
-});
-</script>
+    <script type="text/javascript">
+        var sub = <?=$this->sub?>;
+        $(".fetchNewAccessBlock<?=$this->id?>").click(function(){
+
+            $.ajax({
+                success: function(html){
+                    $(".unique<?=$this->id?>").append(html);
+                    sub ++;
+                },
+                type: 'get',
+                url: '<?=app()->createUrl('ajax/get/getAccessBlock', array(
+                    'name' => $this->name,
+                    'id' => $this->id,
+                    'sub' => '',
+                    ))?>' + sub,
+                cache: false,
+                dataType: 'html'
+            });
+    });
+    </script>
 
 </div>
