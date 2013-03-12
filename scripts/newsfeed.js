13bdb7cfeffd55ec0987122f451917b6f432212f
Kristian Våge <krisvaage@gmail.com>
2013-03-12 19:06:02 +0100

    Infinite scroll uten extensions

--- scripts/newsfeed.js
+++ scripts/newsfeed.js
@@ -30,3 +30,28 @@ require([], function () {
 	});
 
 });
+
+
+/**
+ * Denne javascripten sjekker hvor langt ned en har scrollet
+ * og trykker på 'fetchNews' - knappen når en er 100 pixler fra
+ * bunnen. Forandre på offset for å forandre hvor langt fra
+ * bunnen en må være.
+ */
+$(document).ready(scrollChecker());
+
+function scrollChecker() {
+    $(window).scroll(function() {
+        if (shouldLoadMore()) {
+            document.getElementById("fetchNews").click();
+        }
+    });
+}
+
+function shouldLoadMore() {
+    var offset = 100;
+    var limit = $(document).height() - $(window).height() - offset;
+    var bottomOfPage = $(window).scrollTop() >= limit;
+    var fetchNewsExists = $('#fetchNews').length > 0;
+    return bottomOfPage && fetchNewsExists;
+}
