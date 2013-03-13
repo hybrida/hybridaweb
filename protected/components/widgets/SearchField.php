19bb7818f48ae79012da7d00fcec20d1b10f569f
Sigurd Holsen <sighol@gmail.com>
2013-03-13 01:42:36 +0100

    Søk: Viser bare søkefelt når man har logget inn

--- protected/components/widgets/SearchField.php
+++ protected/components/widgets/SearchField.php
@@ -2,6 +2,7 @@
 
 class SearchField extends CWidget {
 	public function run() {
+		if (user()->isGuest) return;
 		?><form class="layout-search">
 			<input type="text" id="searchField" placeholder="Søk"/>
 		</form><?
