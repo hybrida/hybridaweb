220a1427df6fffde3cf1b8dbb99b5b5fd8f83dc7
Sigurd Holsen <sighol@gmail.com>
2013-03-13 08:42:45 +0100

    search: Nå skjer det ingen ting om du trykker på enter når du søker

--- protected/components/widgets/SearchField.php
+++ protected/components/widgets/SearchField.php
@@ -3,8 +3,8 @@
 class SearchField extends CWidget {
 	public function run() {
 		if (user()->isGuest) return;
-		?><form class="layout-search">
+		?><div class="layout-search">
 			<input type="text" id="searchField" placeholder="Søk"/>
-		</form><?
+		</div><?
 	}
 }
