6beb455d0c69b8baabe650c3cb0042ccfd48e6cd
Sigurd Holsen <sighol@gmail.com>
2013-04-02 15:35:14 +0200

    Controller: Fjernet gamle og gale kommentarer

--- protected/components/Controller.php
+++ protected/components/Controller.php
@@ -7,21 +7,16 @@
 class Controller extends CController {
 
 	/**
-	 * @var string the default layout for the controller view. Defaults to '//layouts/column1',
-	 * meaning using a single column layout. See 'protected/views/layouts/column1.php'.
+	 * @var string the default layout for the controller view
 	 */
 	public $layout = '//layouts/singleColumn';
 
 	/**
 	 * @var array context menu items. This property will be assigned to {@link CMenu::items}.
+	 * Does only work if the layout is set to //layouts/crud.
 	 */
 	public $menu = array();
 
-	/**
-	 * @var array the breadcrumbs of the current page. The value of this property will
-	 * be assigned to {@link CBreadcrumbs::links}. Please refer to {@link CBreadcrumbs::links}
-	 * for more details on how to specify this property.
-	 */
 	public $breadcrumbs = array();
 	public $breadcrumbOptions = array();
 	public $pdo;
