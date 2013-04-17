d0befb156400303ae53e4cef6a078a7a48016406
Teodor Lunaas Heggelund <teodor.heggelund@gmail.com>
2013-04-17 08:16:09 +0200

    Bruk av activeRecord

--- protected/views/quiz/status.php
+++ protected/views/quiz/status.php
@@ -5,10 +5,17 @@
 	<li><?=$team->name?>: <?=$team->foundedDate?></li>
 	
 	<ul>
-		<? foreach ($team->quizTeamScores as $score):?>
-		<li><? debug($team->quizTeamScores) ?></li>
-		<? endforeach; ?>
+		<? /* foreach ($team->quizTeamScores as $score):?>
+		<li><? debug($team->quizTeamScores) ?></li>~
+		<? endforeach; */ ?>
 	</ul>
-	
 <? endforeach; ?>
 </ul>
+
+<p>
+	<? $utr = QuizTeam::model()->findByPk('1'); ?>
+	Laget <?=$utr->name?> ble starta <?=$utr->foundedDate?>.
+	<? foreach($utr->quizTeamMembers as $member): ?>
+	<p><? print_r($member) ?></p>
+	<? endforeach; ?>
+</p>
