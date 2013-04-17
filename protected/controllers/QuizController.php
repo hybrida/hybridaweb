5756f502b9dc0bd7fc3e2e158e00819ff6580d9d
Teodor Lunaas Heggelund <teodor.heggelund@gmail.com>
2013-04-17 08:15:44 +0200

    Prøver å regne ut scoren til et lag

--- protected/controllers/QuizController.php
+++ protected/controllers/QuizController.php
@@ -2,11 +2,32 @@
 
 class QuizController extends Controller {
 	
+	// Oversikt over alle lag med poengsum
 	public function actionIndex() {
 		$teams = QuizTeam::model()->findAll();
-
+		
+		foreach ($teams as $team) {
+			debug($team);
+			echo '<br>';
+			
+			
+			$totalScore = 0;
+			$teamScores = $team->quizTeamScores;
+			debug($teamScores);
+			foreach ($teamScores as $scoreObject) {
+				die();
+				$totalScore += $scoreObject->score;
+			}
+			echo $totalScore;
+		}
+		
+		return;
 		$this->render("status", array(
 			'quizTeams' => $teams,
 		));
+	}
+	
+	private function getTeamScore($team) {
+		
 	}
 }
