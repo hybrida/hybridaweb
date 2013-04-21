3bf81173bb9051b44096c6da22edd64f114a24d9
Sigurd Holsen <sighol@gmail.com>
2013-04-21 22:53:36 +0200

    La til RSS-feed for nyhetsfeeden

--- protected/controllers/NewsfeedController.php
+++ protected/controllers/NewsfeedController.php
@@ -31,4 +31,37 @@ class NewsfeedController extends Controller {
 		return $elements;
 	}
 
+	public function actionAtom($limit=30) {
+		Yii::import('ext.efeed.*');
+		$criteria = new CDbCriteria();
+		$criteria->limit = $limit;
+		$criteria->order = "weight DESC, timestamp DESC";
+		$models = News::model()->findAll($criteria);
+
+		$feed = new EFeed(EFeed::ATOM);
+
+		// IMPORTANT : No need to add id for feed or channel.
+		// It will be automatically created from link.
+		$feed->title = 'Hybrida Nyhetsstrøm';
+		$feed->link = 'http://hybrida.no';
+
+		$feed->addChannelTag('updated', date(DATE_ATOM, time()));
+		$feed->addChannelTag('author', array('name'=>'Hybrida'));
+
+		$baseUrl = "http://hybrida.no";
+		foreach ($models as $model) {
+			$item = $feed->createNewItem();
+
+			$item->title = $model->title;
+			$item->link  = $baseUrl . $model->viewUrl;
+			// we can also insert well formatted date strings
+			$item->date = $model->timestamp;
+			$item->description = $model->ingress;
+
+			$feed->addItem($item);
+		}
+
+		$feed->generateFeed();
+	}
+
 }
