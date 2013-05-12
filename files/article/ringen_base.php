3f4e664701229162c807b40efafd0dfc7e9f8a02
Oda Enger Hoem <odaeh@stud.ntnu.no>
2013-05-12 17:22:42 +0200

    fjernet gammel I&IKT-ringen logo

--- files/article/ringen_base.php
+++ files/article/ringen_base.php
@@ -6,11 +6,6 @@
  	<h1><?= $article->title ?></h1>
  </div>
  <div id="article-content">
-	<p>
-		<img alt="IKT-ringen logo"
-			 width="256" 
-			 src="/upc/files/ringen/images/iktlogo_planet.png" />
-	</p>
  	<?= $article->content ?>
 </div>
 </div>
