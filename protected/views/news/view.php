4209c1e07bdf804b4e07d9a17915208b9561c3aa
Håkon Bråten <haakobra@stud.ntnu.no>
2013-05-13 10:39:30 +0200

    Fikset kræsj ved artikler uten signup

--- protected/views/news/view.php
+++ protected/views/news/view.php
@@ -39,16 +39,18 @@ $this->renderPartial('_signup_sidebar', array(
 	'isAttending' => $isAttending,
 ));?>
 
-<div class="g-barTitle">Tilgang</div>
-<div class="g-barText">
-	<? $tilgang = Access::toText($signup->access); ?>
-	<? if($tilgang == ""): ?>
-		Alle har tilgang
-	<? else: ?>
-		Disse kan melde seg på:<br>
-		<?= $tilgang ?>
-	<? endif ?>
-</div>
+<? if($signup !== null): ?>
+	<div class="g-barTitle">Tilgang</div>
+	<div class="g-barText">
+		<? $tilgang = Access::toText($signup->access); ?>
+		<? if($tilgang == ""): ?>
+			Alle har tilgang
+		<? else: ?>
+			Disse kan melde seg på:<br>
+			<?= $tilgang ?>
+		<? endif ?>
+	</div>
+<? endif ?>
 
 <?
 $this->widget('application.components.widgets.ActivitiesFeed');
