b6a57517a0906f59b93ebc4149884277da80eb52
Sigurd Holsen <sighol@gmail.com>
2013-06-19 23:48:25 +0200

    profil: Fikset litt på designet på profilsiden

--- protected/views/profile/info.php
+++ protected/views/profile/info.php
@@ -6,62 +6,62 @@ $this->renderPartial('_header', array(
 	'user' => $user,
 ))
 ?>
-
-<table>
-    <tr>
-		<td>NTNU e-post:         </td>
-		<td><?= CHtml::mailto($user->username . "@stud.ntnu.no") ?></td>
-	</tr>
-    <tr>
-		<td>Alternativ e-post:   </td>
-		<td> <?= CHtml::mailto($user->altEmail) ?></td>
-	</tr>
-    <tr>
-		<td>Hjemmeside: </td>
-		<td><? 
-			$address = "http://folk.ntnu.no/" . $user->username;
-			echo CHtml::link($address, $address) 
-		?></td>
-    <tr>
-		<td>Telefon: </td>
-		<td> <?= $user->phoneNumber ?></td>
-	</tr>
-	
-	<? if ($user->gender != "unknown") { ?>
-    <tr>
-		<td>Kjønn: </td>
-		<td> <?= $user->getGenderInNorwegian() ?></td>
-	</tr>
-	<? } ?>
-	
-	<? if ($user->birthdate != '0000-00-00'): ?>
-		<tr>
-			<td>Fødselsdato: </td>
-			<td> <?= Html::dateToString($user->birthdate) ?></td>
+<div class="profileInfo">
+	<table class="infoTable">
+	    <tr>
+			<th>NTNU e-post:</th>
+			<td><?= CHtml::mailto($user->username . "@stud.ntnu.no") ?></td>
 		</tr>
+	    <tr>
+			<th>Alternativ e-post:</th>
+			<td> <?= CHtml::mailto($user->altEmail) ?></td>
+		</tr>
+	    <tr>
+			<th>Hjemmeside:</th>
+			<td><?
+				$address = "http://folk.ntnu.no/" . $user->username;
+				echo CHtml::link($address, $address)
+			?></td>
+	    <tr>
+			<th>Telefon:</th>
+			<td> <?= $user->phoneNumber ?></td>
+		</tr>
+
+		<? if ($user->gender != "unknown") { ?>
+	    <tr>
+			<th>Kjønn:</th>
+			<td> <?= $user->getGenderInNorwegian() ?></td>
+		</tr>
+		<? } ?>
+
+		<? if ($user->birthdate != '0000-00-00'): ?>
+			<tr>
+				<th>Fødselsdato:</th>
+				<td> <?= Html::dateToString($user->birthdate) ?></td>
+			</tr>
+		<? endif ?>
+
+	    <tr>
+			<th>Spesialisering:</th>
+			<td><?= $user->specialization ? $user->specialization->name : ""?></td>
+		</tr>
+	    <tr>
+			<th>Avgangsår:</th>
+			<td><?=CHtml::link($user->graduationYear, array('/students/view', 'id' => $user->graduationYear))?></td>
+		</tr>
+	    <tr>
+			<th>Medlemskap: </th>
+			<td> <?= ($user->member == "true" ? "Medlem" : "Ikke Medlem") ?></td>
+		</tr>
+
+	</table>
+	<article class="userDescription">
+		<?= $user->description ?>
+	</article>
+
+	<? if($user->linkedin): ?>
+	    <h2>LinkedIn:</h2>
+	    <script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
+	    <script type="IN/MemberProfile" data-id="http://www.linkedin.com/<?= $user->linkedin ?>" data-format="inline"></script>
 	<? endif ?>
-		
-    <tr>
-		<td>Spesialisering:     </td>
-		<td><?= $user->specialization ? $user->specialization->name : ""?></td>
-	</tr>
-    <tr>
-		<td>Avgangsår:          </td>
-		<td><?=CHtml::link($user->graduationYear, array('/students/view', 'id' => $user->graduationYear))?></td>
-	</tr>
-    <tr>
-		<td>Medlemskap:         </td>
-		<td> <?= ($user->member == "true" ? "Medlem" : "Ikke Medlem") ?></td>
-	</tr>
-
-    <tr>
-		<td>Beskrivelse:        </td>
-		<td> <?= $user->description ?></td>
-	</tr>
-</table>
-
-<? if($user->linkedin): ?>
-    <h2>LinkedIn:</h2>
-    <script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
-    <script type="IN/MemberProfile" data-id="http://www.linkedin.com/<?= $user->linkedin ?>" data-format="inline"></script>
-<? endif ?>
+</div>
