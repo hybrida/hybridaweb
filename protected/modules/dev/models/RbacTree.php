e3208099f2203af149247f6772150e9b7557c5d4
Sigurd Holsen <sighol@gmail.com>
2013-06-22 12:05:19 +0200

    dev: Mye bedre rbactre

--- protected/modules/dev/models/RbacTree.php
+++ protected/modules/dev/models/RbacTree.php
@@ -3,8 +3,14 @@
 class RbacTree extends CComponent {
 
 	private $list;
+	private $items;
 
 	public function __construct() {
+		$this->getItemChildren();
+		$this->getItems();
+	}
+
+	private function getItemChildren() {
 		$sql = "SELECT * FROM rbac_itemchild";
 		$stmt = Yii::app()->db->pdoInstance->prepare($sql);
 		$stmt->execute();
@@ -21,16 +27,38 @@ class RbacTree extends CComponent {
 		ksort($this->list);
 	}
 
-	public function getTree($parent) {
-		$que = $this->list[$parent];
-		$children = array();
-		while (count($que) > 0) {
-			$element = array_pop($que);
-			$que += $this->getChildren($element);
-			$children[] = $element;
+	private function getItems() {
+		$sql = "SELECT * FROM rbac_item";
+		$stmt = Yii::app()->db->pdoInstance->prepare($sql);
+		$stmt->execute();
+		$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		foreach ($data as $assocItem) {
+			$bizRule = $assocItem['bizrule'];
+			$name = $assocItem['name'];
+			$item = new RbacItem($name, $bizRule);
+			$this->items[] = $item;
 		}
-		asort($children);
-		return $children;
+	}
+
+	public function getTree($parent) {
+		$root = new TreeItem($parent);
+		$que = array($root);
+		while(count($que) > 0) {
+			$current = array_pop($que);
+			$rbacItem = $this->getRbacItem($current->name);
+			$current->bizRule = $rbacItem->bizRule;
+			$children = $this->getChildren($current->name);
+			foreach ($children as $child) {
+				$item = new TreeItem($child);
+				$que[] = $item;
+				$current->children[] = $item;
+			}
+		}
+		return $root;
+	}
+
+	private function makeRed($name) {
+		return "<div style='color: red'>$name</div>";
 	}
 
 	private function getChildren($parent) {
@@ -41,9 +69,41 @@ class RbacTree extends CComponent {
 		}
 	}
 
+	private function getRbacItem($search) {
+		foreach ($this->items as $item) {
+			if ($item->name == $search) {
+				return $item;
+			}
+		}
+		throw new CException("Fant ikke til $search");
+	}
+
 	public function getAll() {
 		return $this->list;
 	}
 
+}
 
+class RbacItem {
+	public $name;
+	public $bizRule;
+
+	public function __construct($name, $bizRule) {
+		$this->name = $name;
+		$this->bizRule = $bizRule;
+	}
+}
+
+class TreeItem {
+	public $name;
+	public $bizRule;
+	public $children = array();
+
+	public function __construct($name) {
+		$this->name = $name;
+	}
+
+	public function hasBizRule() {
+		return $this->bizRule != null && $this->bizRule != "";
+	}
 }
--- protected/modules/dev/views/default/rbactree.php
+++ protected/modules/dev/views/default/rbactree.php
@@ -1,14 +1,33 @@
-<style>
-	.left {
-		float: left;
-		width: 40%;
-	}
+<?php function printList($node) {?>
 
-	.right {
-		float: right;
-		width: 40%;
-	}
-</style>
+	<li>
+		<? if ($node->hasBizRule()): ?>
+			<div class="g-infoBox">
+				<span style="color: red"><?= $node->name ?></span>
+				<div class="g-infoBoxContent" style="width: 600px">
+					<?
+					$source =  highlight_string("<?php " . $node->bizRule . " ?>", true);
+					$pattern = "/&nbsp;/";
+					$out = preg_replace($pattern, " ", $source);
+					?>
+					<?= $out ?>
+				</div>
+			</div>
+		<? else:  ?>
+			<span><?= $node->name ?></span>
+		<? endif ?>
+
+		<? if (count($node->children) > 0): ?>
+			<?php foreach ($node->children as $child): ?>
+				<ul>
+					<? printList($child)  ?>
+				</ul>
+			<?php endforeach ?>
+		<? endif ?>
+	</li>
+
+<? }  ?>
+
 
 <h1>RbacTree: <?= $parent ?></h1>
 
@@ -16,9 +35,7 @@
 
 <div class="right">
 	<ul>
-		<? foreach ($tree as $child): ?>
-			<li><?= $child ?></li>
-		<? endforeach ?>
+		<? printList($tree) ?>
 	</ul>
 </div>
 
