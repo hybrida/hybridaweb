d32b2cdab41ce7178744e329ca269c50b1faaec8
Sigurd Holsen <sighol@gmail.com>
2013-08-24 16:58:29 +0200

    knightPresentation: fikset litt på javascripten

--- protected/components/widgets/views/knightPresentation.php
+++ protected/components/widgets/views/knightPresentation.php
@@ -1,36 +1,22 @@
+ <div id="widget-knightPresentation">
 
-    <div id="knightPresentation_wrapper">
-        <div id="knightPresentation_content">
-                <div id="knightPresentation_content-left">Navn</div>
-                <div id="knightPresentation_content-right">Tildelingsår</div>
-                <div id="knightPresentation_content-main">Avgangsår</div>
-         </div>
-    </div>
-
-    
-<?foreach ($models as $model) :?>
-
-
-       
-        <div id="knightPresentation_wrapper">
-            <div id="knightPresentation_content">
-                    <div id="knightPresentation_content-left"><a href="javascript:toogle('knightPresentation_footer');"><?=$model->firstName;?> <?=$model->lastName;?></a></div>
-                    <div id="knightPresentation_content-main"><?=$model->graduationYear;?></div>
-                    <div id="knightPresentation_content-right"><?=$model->grantYear;?></div>
-            </div>
-            
-            <div style="none" id="knightPresentation_footer" class="knightPresentation_hidden"><?=$model->reason?></div>
+    <?foreach ($models as $model) :?>
+        <div class="knight g-clearfix">
+            <div class="grantYear"><?= $model->grantYear ?></div>
+            <div class="name"><?= $model->firstName . " " . $model->lastName ?></div>
+            <div class="reason"><?= $model->reason ?></div>
         </div>
+    <?endforeach;?>
 
     <script type="text/javascript">
-        function toggle(element) {
-            document.getElementById(element).style.display = (document.getElementById(element).style.display == "none") ? "" : "none";
-        }
+
+        var knights = $("#widget-knightPresentation").find(".knight");
+        knights.find(".name").click(function(e) {
+           var knight = $(e.currentTarget);
+           var parent = knight.parent();
+           var reason = parent.find(".reason").toggle();
+        });
+
     </script>
 
-
-<?endforeach;?>
-
-
-
-    
+</div>
