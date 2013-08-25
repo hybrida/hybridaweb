bc89ca9c0d403b4f7782b92c2d2c70de4371a2fb
Sigurd Holsen <sighol@gmail.com>
2013-08-25 13:39:18 +0200

    lesscompiler: forbedring

--- protected/commands/LessCommand.php
+++ protected/commands/LessCommand.php
@@ -13,25 +13,54 @@ class LessCommand extends CConsoleCommand {
 		$this->lessDir = dirname(Yii::getPathOfAlias("webroot")) . "/style/less/";
 		$this->cssDir = dirname(Yii::getPathOfAlias("webroot")) . "/style/css/";
 		$this->styleDir = dirname(Yii::getPathOfAlias("webroot")) . "/style/";
+
 		$this->compiler = Yii::app()->lessCompiler;
-		$this->getLessCode();
 	}
 
-	public function getLessCode() {
+	private function getLessFiles() {
 		$files = scandir($this->lessDir);
-		$files = array_filter($files, function($el){
+		return array_filter($files, function($el){
 			return !($el == "." || $el == "..");
 		});
+	}
+
+	public function writeMainCss() {
+		$files = $this->getLessFiles();
 		$cssCode = array();
 		foreach ($files as $file) {
 			$cssCode[] = $this->compiler->compile($this->lessDir . $file);
 		}
-
 		$mainCss = file_put_contents($this->styleDir . "main.css", implode("\n", $cssCode));
 	}
 
+	public function writeAllCssFiles() {
+		$files = $this->getLessFiles();
+		$cssCode = array();
+		foreach ($files as $file) {
+			$cssName = str_replace(".less", ".css", $file);
+			$this->compiler->compileFile($this->lessDir . $file, $this->cssDir . $cssName);
+		}
+	}
+
 	public function run($args) {
-		cdebug($args);
+		if (in_array("compress", $args)) {
+			$this->writeMainCss();
+		} elseif (in_array("singles", $args)) {
+			$this->writeAllCssFiles();
+		} else {
+			$this->help();
+		}
+	}
+
+	private function help() {
+		$helpText = <<<EOT
+HOWTO:
+less compress:
+	Lag _en_ komprimert main.css fil. Denne som brukes i produksjon.
+less singles:
+	Lag en css-fil for alle less-filene. Denne kan brukes i debugging.
+EOT;
+		echo $helpText;
 	}
 
 }
