a8a7b0da330435ce2e08e66271f2d66db101e40a
Sigurd Holsen <sighol@gmail.com>
2013-08-25 15:07:07 +0200

    lesscompiler: bugfix

--- protected/extensions/lessphp/LessCompiler.php
+++ protected/extensions/lessphp/LessCompiler.php
@@ -4,18 +4,17 @@ require dirname(__FILE__) . "/lessc.inc.php";
 
 class LessCompiler extends CComponent {
 
-	private $lessc;
-
 	public function init() {
-		$this->lessc = new lessc;
 	}
 
 	public function compileFile($fname, $outFname = null) {
-		return $this->lessc->compileFile($fname, $outFname);
+		$l = new lessc;
+		return $l->compileFile($fname, $outFname);
 	}
 
 	public function compile($string, $name = null) {
-		return $this->lessc->compileFile($string, $name);
+		$l = new lessc;
+		return $l->compileFile($string, $name);
 	}
 
 }
