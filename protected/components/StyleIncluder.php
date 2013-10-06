72792f0711f533e626fb8ddfa80fc7ff7dc4ac1c
Sigurd Holsen <sighol@gmail.com>
2013-10-06 15:25:28 +0200

    Fjernet less og bytta til vanlig css

--- protected/components/StyleIncluder.php
+++ protected/components/StyleIncluder.php
@@ -9,22 +9,9 @@ class StyleIncluder {
 		self::registerDirectory("style/css", self::$cssFiles);
 		$output = "";
 		foreach (self::$cssFiles as $file) {
+			if (strpos($file, "min.css") !== false) continue;
 			$output .= CHtml::tag('link', array(
 				'rel' => 'stylesheet',
-				'type' => 'text/css',
-				'href' => $file,
-			)) . PHP_EOL;
-
-		}
-		return $output;
-	}
-
-	public static function printLessTags() {
-		self::registerDirectory("style/less", self::$lessFiles);
-		$output = "";
-		foreach (self::$lessFiles as $file) {
-			$output .= CHtml::tag('link', array(
-				'rel' => 'stylesheet/less',
 				'type' => 'text/css',
 				'href' => $file,
 			)) . PHP_EOL;
--- themes/hybrida/views/layouts/_css.php
+++ themes/hybrida/views/layouts/_css.php
@@ -1,9 +1,5 @@
 		<? if (YII_DEBUG): ?>
-			<?= StyleIncluder::printLessTags() ?>
-			<script type="text/javascript">
-				less = { env: 'development' };
-			</script>
-			<script src="/scripts/less.min.js"></script>
+			<?= StyleIncluder::printCssTags() ?>
 		<? else: ?>
 			<link rel="stylesheet" type="text/css" href="<?= Yii::app()->request->baseUrl ?>/style/css/min.css" />
 		<? endif ?>
