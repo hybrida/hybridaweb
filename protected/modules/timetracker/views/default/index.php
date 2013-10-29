d6bed19419509b2aa48a669b3737c53990ccf9f1
Sigurd Holsen <sighol@gmail.com>
2013-10-29 15:04:57 +0100

    timetracker: motivational quotes

--- protected/modules/timetracker/views/default/index.php
+++ protected/modules/timetracker/views/default/index.php
@@ -4,6 +4,25 @@
 $this->breadcrumbs=array(
 	$this->module->id,
 );
+
+$motivational_quotes = array(
+    'Det er DU som bestemmer over DIN tid!',
+    "Det viktigste er ikke å vinne, men å delta",
+    "Husk at du bare konkurrer med deg selv!",
+    "Be the person you want to live your life as![sic]",
+    'To hell with circumstances; I create opportunities." – Bruce Lee',
+    '"Time is the coin of life. Only you can determine how it will be spent." – Carl Sandburg',
+    '"I get knocked down. But I get up again. You’re never going to keep me down." – Chumbawamba',
+    '"Character is the result of two things: mental attitude and the way we spend our time." – Elbert Green Hubbard',
+    '"When it’s time to die, let us not discover that we have never lived." -Henry David Thoreau',
+    '"Nothing contributes so much to tranquilize the mind as a steady purpose– a point on which the soul may fix its intellectual eye." – Mary Shelley',
+    '"There is no such thing as failure. There are only results." – Tony Robbins',
+    '"To different minds, the same world is a hell, and a heaven." – Ralph Waldo Emerson',
+    '"It’s great to be great, but its greater to be human." – Will Rogers',
+    '"Do not wait to strike till the iron is hot; but make it hot by striking." – William B. Sprague ',
+);
+$quote = $motivational_quotes[array_rand($motivational_quotes)];
+
 ?>
 
 <div id="timetrackerIndex">
@@ -26,7 +45,7 @@ $this->breadcrumbs=array(
                 x: -20 //center
             },
             subtitle: {
-                text: 'Husk at du bare konkurrer med deg selv!',
+                text: '<?= $quote ?>',
                 x: -20
             },
             yAxis: {
