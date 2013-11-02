f0b6656761e0de51468bda37e381e081ef01b144
Teodor Heggelund <teodor.heggelund@gmail.com>
2013-11-02 05:49:25 +0100

    Redirecting to innsida logout on logout. Allows the user to remove any cached login data from innsida.

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -53,6 +53,6 @@ class SiteController extends Controller {
 
 	public function actionLogout() {
 		Yii::app()->user->logout();
-		$this->redirect(Yii::app()->homeUrl);
+		$this->redirect("https://innsida.ntnu.no/c/portal/logout");
 	}
 }
