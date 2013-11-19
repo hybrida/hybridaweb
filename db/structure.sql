c6836b41e790ba7fef10ba71ddfbd8d5811b9094
Kevin Robert Stravers <kevinrs@stud.ntnu.no>
2013-11-19 19:15:43 +0100

    La til filer som ender på ~ til gitignore

--- db/structure.sql
+++ db/structure.sql
@@ -6,6 +6,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
+
 CREATE DATABASE IF NOT EXISTS `hybrida_dev` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 USE `hybrida_dev`;
 
