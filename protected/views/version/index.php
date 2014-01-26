9d8971aef7cc7dca1b111924ed6c5da070833ba4
Sigurd Holsen <sighol@gmail.com>
2014-01-26 22:23:01 +0100

    La til VersionController.php for å kunne se hvilken versjon som er i produksjon

---
+++ protected/views/version/index.php
@@ -0,0 +1,9 @@
+<div class="versionIndex">
+	<h1>Versjon</h1>
+	<p class="hash">
+		<strong><?= $hash ?></strong>
+	</p>
+
+	Du kan gå inn på <a href="<?= $url ?>">github</a> for å se revisjonen.
+
+</div>
