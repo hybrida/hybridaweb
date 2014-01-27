565817e53620715b7c214f3e72246178bf4bc93b
Marius Røed <mariuroe@stud.ntnu.no>
2014-01-27 21:12:28 +0100

    KnightPresentation: Formattering

--- protected/components/widgets/KnightPresentation.php
+++ protected/components/widgets/KnightPresentation.php
@@ -1,22 +1,23 @@
 <?php
+
 /**
  * Description of TabNavigation
  *
  * @author marius
  */
 class KnightPresentation extends CWidget {
-    
-        private $models;
-	
-	public function init() {
-		parent::init();  
-                $this->models = Knight::model()->findAll();
-	}
-	
-	public function run() {
-		$this->render('knightPresentation', array(
-                    'models' => $this->models,
-                ));
-	}
+
+    private $models;
+
+    public function init() {
+        parent::init();
+        $this->models = Knight::model()->findAll();
+    }
+
+    public function run() {
+        $this->render('knightPresentation', array(
+            'models' => $this->models,
+        ));
+    }
 
 }
