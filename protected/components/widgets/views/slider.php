40e061d48f038797043332417c0817084d8f81da
Marius Røed <mariuroe@stud.ntnu.no>
2014-01-28 22:29:31 +0100

    Slider: Fikset slik at navigasjonsiconer kommer opp på slideren, og ikke under.

--- protected/components/widgets/views/slider.php
+++ protected/components/widgets/views/slider.php
@@ -4,6 +4,8 @@
     <?= FrontpageBanner::getBanner() ?>
     <img src="/upc/images/frontpage_banner/banner.png">
     <img src="/upc/images/frontpage_banner/banner2.png">
+    <span class="slidesjs-previous slidesjs-navigation"><a href="#" class="slidesjs-previous slidesjs-navigation"><i class="icon-chevron-left icon-large"></i></a></span>
+    <span class="slidesjs-next slidesjs-navigation"><a href="#" class="slidesjs-next slidesjs-navigation"><i class="icon-chevron-right icon-large"></i></a></span>
 </div>
 
 <script>
@@ -11,19 +13,20 @@
     $("#slides").slidesjs({
       width: 1000,
       height: 250,
+      navigation: true,
       play: {
       active: false,
         // [boolean] Generate the play and stop buttons.
         // You cannot use your own buttons. Sorry.
       effect: "slide",
         // [string] Can be either "slide" or "fade".
-      interval: 5000,
+      interval: 7000,
         // [number] Time spent on each slide in milliseconds.
       auto: true,
         // [boolean] Start playing the slideshow on load.
       swap: false,
         // [boolean] show/hide stop and play buttons
-      pauseOnHover: false,
+      pauseOnHover: true,
         // [boolean] pause a playing slideshow on hover
       restartDelay: 2500
         // [number] restart delay on inactive slideshow
