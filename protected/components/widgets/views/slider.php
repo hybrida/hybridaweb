71b686ba64676b669057f5e80151fcd7da1c1308
Marius Røed <mariuroe@stud.ntnu.no>
2014-02-02 21:39:08 +0100

    Slider: Endret slik at banner kan bruker midlertidig.

--- protected/components/widgets/views/slider.php
+++ protected/components/widgets/views/slider.php
@@ -2,7 +2,6 @@
 
 <div id="slides">
     <?= FrontpageBanner::getBanner() ?>
-    <img src="/upc/images/frontpage_banner/banner.png">
     <img src="/upc/images/frontpage_banner/banner2.png">
     <span class="slidesjs-previous slidesjs-navigation"><a href="#" class="slidesjs-previous slidesjs-navigation"><i class="icon-chevron-left icon-large"></i></a></span>
     <span class="slidesjs-next slidesjs-navigation"><a href="#" class="slidesjs-next slidesjs-navigation"><i class="icon-chevron-right icon-large"></i></a></span>
