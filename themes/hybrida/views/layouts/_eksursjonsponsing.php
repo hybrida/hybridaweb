2af35e3657956726f73196701337d2582d26d637
Sigurd Holsen <sighol@gmail.com>
2014-02-18 16:07:15 +0100

    La til eksursjonsponsing

---
+++ themes/hybrida/views/layouts/_eksursjonsponsing.php
@@ -0,0 +1,5 @@
+<div class="g-barTitle">Eksursjons-sponsorer</div>
+<div class="g-barText">
+	<a href="http://www.skretting.no/"><?= Image::tag(363, 'sidebar') ?></a>
+	<a href="http://www.steria.com/no/"><?= Image::tag(362, 'sidebar') ?></a>
+</div>
