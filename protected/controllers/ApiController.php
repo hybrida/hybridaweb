c7e706469baca0038650bec767fb157d3c556b56
Sigurd Holsen <sighol@gmail.com>
2014-03-21 11:25:35 +0100

    Ryddet litt på formattering

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -1,22 +1,20 @@
 <?php
 
 class ApiController extends Controller {
-	
+
 	public function actionAddGossip() {
 		$newGossip = $_POST['gossipText'];
 		$gossip = new Gossip;
-		
+
 		$gossip->gossipText = $newGossip;
 		$gossip->save();
 	}
-	
+
 	public function actionShowGossip() {
 		$gossips = Gossip::model()->findAll();
-		
+
 		$this->render("gossip", array(
 			'gossipText' => $gossips,
 		));
 	}
 }
-
-?>
