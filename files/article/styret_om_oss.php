803b9814a2db88f384bfd02d393aed68f4cb420f
Øystein Hauan <oysteibh@stud.ntnu.no>
2014-05-23 18:07:45 +0200

    Endret detailz

--- files/article/styret_om_oss.php
+++ files/article/styret_om_oss.php
@@ -37,8 +37,8 @@
 			<div class="styretmedlem">
 				<h2>Leder</h2>
 				<img src="/upc/files/styret/artikkel/leder.jpg" alt ="Leder av Hybrida" />
-				<a href="/profil/aasmunph">&Aring;smund Pedersen Hugo</a>
-				3. kl
+				<a href="/profil/simenlil">Simen Lillehagen</a>
+				4. kl
 				<div class="description">
 					Har det overordnede ansvaret i Hybrida, og er rett og slett helt sjef.
 				</div>
@@ -47,8 +47,8 @@
 			<div class="styretmedlem">
 				<h2>Nestleder</h2>
 				<img src="/upc/files/styret/artikkel/nestleder.jpg" alt ="Leder av Hybrida" />
-				<a href="/profil/hannegje">Hanne Gjemdal</a>
-				3. kl
+				<a href="/profil/stephabu">Stephanie Buadu</a>
+				2. kl
 				<div class="description">
 					Samarbeider med lederen om det organisatoriske og administrative ansvaret i 
 					styret.
@@ -59,7 +59,7 @@
 				<h2>Skattmester</h2>
 				<img src="/upc/files/styret/artikkel/skattemester.jpg" alt ="Leder av Hybrida" />
 				<a href="/profil/astridnd">Astrid Nerhus Dale</a>
-				1. kl
+				2. kl
 				<div class="description"></div>
 				Ansvaret for pengestr&oslash;mmen inn og, 
 				sannsynligvis mest, ut.
@@ -79,8 +79,8 @@
 			<div class="styretmedlem">
 				<h2>Bedriftskomit&eacute;sjef</h2>
 				<img src="/upc/files/styret/artikkel/bksjef.jpg" alt ="Leder av Hybrida" />
-				<a href="/profil/simenlil">Simen Lillehagen</a>
-				3. kl
+				<a href="/profil/andreamf">Andre Marie Festøy</a>
+				2. kl
 				<div class="description">
 					Fikser og ordner med profileringen av linja og linjeforeningen utad til
 					bedrifter og n&aelig;ringslivet generelt.
@@ -91,7 +91,7 @@
 				<h2>Vevsjef</h2>
 				<img src="/upc/files/styret/artikkel/vevsjef.jpg" alt ="Leder av Hybrida" />
 				<a href="/profil/oysteibh">&Oslash;ystein Hauan</a>
-				1. kl
+				2. kl
 				<div class="description">
 					Ansvar for utvikling og drift av nettsidene, samt informasjonsflyten i
 					linjeforeningen. 
@@ -101,8 +101,8 @@
 			<div class="styretmedlem">
 				<h2>Jentekomit&eacute;sjef</h2>
 				<img src="/upc/files/styret/artikkel/jentekomsjef.jpg" alt ="Leder av Hybrida" />
-				<a href="/profil/siljemol">Silje M&oslash;ller</a>
-				3. kl
+				<a href="/profil/helendyp">Helene Dypbukt</a>
+				2. kl
 				<div class="description">
 					Sjef for jentekomiteen.
 				</div>
