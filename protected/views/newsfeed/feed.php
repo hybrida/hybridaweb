1122df152a8ca8d93b743834b6cdb64a1a5ef93c
Øystein Hauan <oysteibh@stud.ntnu.no>
2014-09-16 19:09:25 +0200

    Endret kontortider

--- protected/views/newsfeed/feed.php
+++ protected/views/newsfeed/feed.php
@@ -37,7 +37,7 @@ $this->beginClip('sidebar'); ?>
         <div class="officeHours">
             <p>
                 <!-- <div class="title">Styret:</div> Torsdag 10:15-12:00<br/>
-                Hybridakontoret, Gamle kjemi
+                Hybridakontoret, Gamle kjemiiiii
                 --><br/><br/>
                 <div class="title">SPR:</div> Torsdag 12.00-14.00<br/>
                 Studentrådskontoret over <br/>kiosken på stripa
