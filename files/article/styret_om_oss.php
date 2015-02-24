2f4e6db67416c7a3509a15fbd6bfb7e64f69b1e1
Ivar H. Krabol <ivarhk@gmail.com>
2015-02-25 00:28:18 +0100

    Updated styre-info

--- files/article/styret_om_oss.php
+++ files/article/styret_om_oss.php
@@ -5,36 +5,57 @@
 <div id="article-content">
 	<style>
 
+        .hele-styret {
+            max-width: 100%;
+        }
+
 		.articleStyret {
 			display: block;
 		}
 
-		.styretmedlemmer {
-			width: 600px;
+		.styremedlemmer div {
+            text-align: center;
+			float: left;
+			width: 300px;
+            height: 470px;
+			margin: 10px 20px;
 		}
 
-		.styretmedlem {
-			float: left;
-			width: 250px;
-			padding: 1em;
-			padding-left: 0;
-		}
-		.styretmedlem img {
+        .styremedlemmer div:nth-child(odd):last-of-type:before {
+            content: ' ';
+            display: block;
+            height: 1px;
+            margin-top: -1px;
+            clear: both;
+        }
+        .styremedlemmer div:nth-child(odd):last-of-type {
+            clear: both;
+            float: none;
+            margin: 10px auto;
+        }
+
+		.styremedlemmer div img {
 			display: block;
-			width: 150px;
+            height: 300px;
+            margin: 0 auto;
 		}
+
+        .styremedlemmer div .description {
+            text-align: left;
+        }
+
 	</style>
 
 	<div class="articleStyret" >
-		<img src="/upc/files/styret/artikkel/styret.jpg" alt="Hele styret" />
+		<img class="hele-styret" src="/upc/files/styret/artikkel/styret.jpg" alt="Hele styret" />
 
 		<p>
 			<strong>Dette er de syv styremedlemmene som per dags dato har hovedansvaret 
 				for &aring; holde liv i linjeforeningen (og dens medlemmer).</strong>
 		</p>
 
-		<div class="styretmedlemmer">
-			<div class="styretmedlem">
+		<div class="styremedlemmer">
+			<div>
 				<h2>Leder</h2>
 				<img src="/upc/files/styret/artikkel/leder.jpg" alt ="Leder av Hybrida" />
 				<a href="/profil/simenlil">Simen Lillehagen</a>
@@ -45,9 +66,9 @@
 				</div>
 			</div>
 
-			<div class="styretmedlem">
+			<div>
 				<h2>Nestleder</h2>
-				<img src="/upc/files/styret/artikkel/nestleder.jpg" alt ="Leder av Hybrida" />
+				<img src="/upc/files/styret/artikkel/nestleder.jpg" alt ="Nestleder" />
 				<a href="/profil/stephabu">Stephanie Buadu</a>
 				, 2. kl
 				<div class="description">
@@ -56,31 +77,32 @@
 				</div>
 			</div>
 
-			<div class="styretmedlem">
+			<div>
 				<h2>Skattmester</h2>
-				<img src="/upc/files/styret/artikkel/skattemester.jpg" alt ="Leder av Hybrida" />
-				<a href="/profil/astridnd">Astrid Nerhus Dale</a>
-				, 2. kl
-				<div class="description"></div>
-				Ansvaret for pengestr&oslash;mmen inn og, 
-				selvfølgelig mest, ut. Driver i følge pålitelige kilder
-				ikke med underslag (men man vet jo aldri). 
+				<img src="/upc/files/styret/artikkel/skattmester.jpg" alt ="Skattmester" />
+				<a href="/profil/sigribra">Sigrid Bratsberg</a>
+				, 1. kl
+				<div class="description">
+                    Ansvaret for pengestr&oslash;mmen inn og,
+                    selvfølgelig mest, ut. Driver i følge pålitelige kilder
+                    ikke med underslag (men man vet jo aldri).
+                </div>
 			</div>
 
-			<div class="styretmedlem">
+			<div>
 				<h2>Festivalus</h2>
-				<img src="/upc/files/styret/artikkel/festivalus.jpg" alt ="Leder av Hybrida" />
-				<a href="/profil/liveja">Live Jacobsen</a>
-				, 3. kl
+				<img src="/upc/files/styret/artikkel/festivalus.jpg" alt ="Festivalus" />
+				<a href="/profil/liveja">Astrid Nerhus Dale</a>
+				, 2. kl
 				<div class="description">
 					Ansvaret for det som blir arrangert av fester, turer og en hel rekke andre
 					arrangementer. Er også daglig leder på Hybridas eksklusive bar på Moholt.  
 				</div>
 			</div>
 
-			<div class="styretmedlem">
+			<div>
 				<h2>Bedriftskomit&eacute;sjef</h2>
-				<img src="/upc/files/styret/artikkel/bksjef.jpg" alt ="Leder av Hybrida" />
+				<img src="/upc/files/styret/artikkel/bksjef.jpg" alt ="Bedkomsjef" />
 				<a href="/profil/andreamf">Andrea Marie Festøy</a>
 				, 2. kl
 				<div class="description">
@@ -90,28 +112,27 @@
 				</div>
 			</div>
 
-			<div class="styretmedlem">
+			<div>
 				<h2>Vevsjef</h2>
-				<img src="/upc/files/styret/artikkel/vevsjef.jpg" alt ="Leder av Hybrida" />
-				<a href="/profil/oysteibh">&Oslash;ystein Hauan</a>
+				<img src="/upc/files/styret/artikkel/vevsjef.jpg" alt ="Vevsjef" />
+				<a href="/profil/ivarhk">Ivar Haga Kr&aring;b&oslash;l</a>
 				, 2. kl
 				<div class="description">
 					Ansvar for utvikling og drift av nettsidene, samt informasjonsflyten i
-					linjeforeningen og HybridLAN. Tar det mye med ro, særlig når det kommer til
-					matte to.  
+					linjeforeningen og HybridLAN. Jau.
 				</div>
 			</div>
 
-			<div class="styretmedlem">
-				<h2>Jentekomit&eacute;sjef</h2>
-				<img src="/upc/files/styret/artikkel/jentekomsjef.jpg" alt ="Leder av Hybrida" />
-				<a href="/profil/helendyp">Helene Dypbukt</a>
-				, 2. kl
-				<div class="description">
-					Sjef for jentekomiteen, eller chicas united som det kalles i spania. 
-					sørger for at damene oppfører seg i henhold. 
-				</div>
-			</div>
+            <div>
+                <h2>Jentekomit&eacute;sjef</h2>
+                <img src="/upc/files/styret/artikkel/jentekomsjef.jpg" alt ="Jentekomsjef" />
+                <a href="/profil/helendyp">Helene Dypbukt</a>
+                , 2. kl
+                <div class="description">
+                    Sjef for jentekomiteen, eller chicas united som det kalles i spania.
+                    sørger for at damene oppfører seg i henhold.
+                </div>
+            </div>
 			<br clear="all" />
 		</div>
 	</div>
