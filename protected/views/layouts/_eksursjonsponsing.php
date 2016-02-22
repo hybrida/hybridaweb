9a1086ca64fd252eca7d2320e8f1403530d7c180
Simen <simennj@outlook.com>
2016-02-22 18:52:58 +0100

    remove old sponsors

--- protected/views/layouts/_eksursjonsponsing.php
+++ protected/views/layouts/_eksursjonsponsing.php
@@ -1,5 +1,7 @@
+<!--
 <div class="g-barTitle">Ekskursjonen</div>
 <div class="g-barText">
 	<a href="http://www.skretting.no/"><?= Image::tag(363, 'sidebar') ?></a>
 	<a href="http://www.steria.com/no/"><?= Image::tag(362, 'sidebar') ?></a>
 </div>
+-->
